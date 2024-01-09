8fa95d9ba2d6084846f5b63c1a94c5ab2d34a4a2
deadlyunicorn <petrache.dev@gmail.com>
2024-01-09 13:33:50 +0200

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeclaTime',
-      darkTheme: ThemeData(
-        colorScheme: const ColorScheme(
-          brightness: Brightness.dark, 
-          primary:  Color( 0xFF2619B4 ), 
-          onPrimary:  Color( 0xFFEAE9FC ), 
-          secondary:  Color(0xFF3B197B), 
-          onSecondary:  Color(0xFFEAE9FC), 
-          error:  Color( 0xFF9E0030 ), 
-          onError:  Color( 0xFFEAE9FC ), 
-          background:  Color( 0xFF040412), 
-          onBackground:  Color( 0xFFEAE9FC ), 
-          surface:  Color( 0xFFB59C0D ), 
-          onSurface:  Color(0xFFEAE9FC)
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Color( 0xFFB59C0D ),
-          foregroundColor: Color(0xFFEAE9FC)
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color(0xFF04041A),
-          selectedItemColor: Color( 0xFFB59C0D )
-        ),
-        useMaterial3: true,
-      ),
+      darkTheme: darkTheme(),
       themeMode: ThemeMode.dark,
       home: const Skeleton(),
+    );
+  }
+
+  ThemeData darkTheme() {
+    return ThemeData(
+      fontFamily: 'Ysabeau',
+      colorScheme: const ColorScheme(
+        brightness: Brightness.dark, 
+        primary:  Color( 0xFF2619B4 ), 
+        onPrimary:  Color( 0xFFEAE9FC ), 
+        secondary:  Color(0xFF3B197B), 
+        onSecondary:  Color(0xFFEAE9FC), 
+        error:  Color( 0xFF9E0030 ), 
+        onError:  Color( 0xFFEAE9FC ), 
+        background:  Color( 0xFF040412), 
+        onBackground:  Color( 0xFFEAE9FC ), 
+        surface:  Color( 0xFFB59C0D ), 
+        onSurface:  Color(0xFFEAE9FC)
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Color( 0xFFB59C0D ),
+        foregroundColor: Color(0xFFEAE9FC)
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Color(0xFF04041A),
+        selectedItemColor: Color( 0xFFB59C0D ),
+      ),
+      useMaterial3: true,
     );
   }
 }
