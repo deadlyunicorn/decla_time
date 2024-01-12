fcd1f483be0249993e103fcbeccbe2499e7f19c5
deadlyunicorn <petrache.dev@gmail.com>
2024-01-12 09:54:48 +0200

    created constants for mapping column numbers

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,14 +4,34 @@ String kApplicationName = 'decla_time';
 
 
 String kName = 'name';
-String kId = 'id';
+const kId = 'id';
 //For api interactions
+const kGuestName = 'guest_name';
 const kArrivalDate = 'arrival_date';
 const kDepartureDate = 'departure_date';
-const kTotalValue = 'total_value';
-const bookingPlatform = 'booking_platform';
+const kPayout = 'payout';
+const kBookingPlatform = 'booking_platform';
 const kSynced     = 'synced';
 const kStatus     = 'status'; 
+const kReservationStatus = "reservation_status"; //used to indicate if it was cancelled or not.
+const kReservationListing = "reservation_listing";
+
+
+const kAirbnbListingNameColumn = 11;
+const kAirbnbIdColumn = 0;
+const kAirbnbGuestNameColumn = 2;
+const kAirbnbArrivalDateColumn = 7;
+const kAirbnbDepartureDateColumn = 8;
+const kAirbnbPayoutColumn = 12;
+const kAirbnbReservationStatusColumn = 1;
+
+const kBookingDotComArrivalDateColumn = 3;
+const kBookingDotComDepartureDateColumn = 4;
+const kBookingDotComGuestNameColumn = 2;
+const kBookingDotComIdColumn = 0;
+const kBookingDotComTotalPriceColumn = 12;
+const kBookingDotComFeeColumn = 14;
+const kBookingDotComReservationStatusColumn = 6;
 
 
 String kPath = 'path';
