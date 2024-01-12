542fc9efb3afb64fbcc1e34bae03985f6e3a8f19
deadlyunicorn <petrache.dev@gmail.com>
2024-01-12 13:57:58 +0200

    Finalized PageSelector widget

--- lib/skeleton/select_page_to_display.dart
+++ lib/skeleton/select_page_to_display.dart
@@ -1,4 +1,7 @@
+import 'package:decla_time/analytics/analytics_page.dart';
 import 'package:decla_time/core/enums/selected_page.dart';
+import 'package:decla_time/declarations/declarations_page.dart';
+import 'package:decla_time/reservations/reservations_page.dart';
 import 'package:flutter/material.dart';
 
 class SelectPageToDisplay extends StatelessWidget {
@@ -9,8 +12,16 @@ class SelectPageToDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+
+    switch( selectedPage ){
+
+      case SelectedPage.reservations:
+        return const ReservationsPage();
+      case SelectedPage.declarations:
+        return const DeclarationsPage();
+      case SelectedPage.analytics:
+        return const AnalyticsPage();
+      
+    }
   }
 }
