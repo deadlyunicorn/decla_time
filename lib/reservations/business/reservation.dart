82f2b01194516ae401addecb9cb7a2bfa88a2395
deadlyunicorn <petrache.dev@gmail.com>
2024-01-12 09:56:03 +0200

    updated the Reservation class

--- lib/reservations/business/reservation.dart
+++ lib/reservations/business/reservation.dart
@@ -1,21 +1,26 @@
-import 'package:decla_time/core/enums/booking_platform.dart';
 
 class Reservation{
+  
+  final String bookingPlatform;
+  final String? listingName;
 
   final String id;
+  final String guestName;
   final DateTime arrivalDate;
   final DateTime departureDate;
-  final double totalValue;
-  final double hostPayout;
-  final BookingPlatform bookingPlatform;
+  final double payout;
+  final String reservationStatus;
 
   Reservation({
+    required this.bookingPlatform,
+    required this.listingName,
+
     required this.id,
+    required this.guestName,
     required this.arrivalDate,
     required this.departureDate,
-    required this.totalValue,
-    required this.hostPayout,
-    required this.bookingPlatform,
+    required this.payout,
+    required this.reservationStatus
     
   }); 
   
