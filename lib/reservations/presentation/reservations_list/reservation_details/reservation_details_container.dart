3eb82f4dd1a7b26fcb2628809a7faa5c8a0982a0
deadlyunicorn <petrache.dev@gmail.com>
2024-01-16 13:21:44 +0200

    Created Reservation Details Container

---
+++ lib/reservations/presentation/reservations_list/reservation_details/reservation_details_container.dart
@@ -0,0 +1,132 @@
+import 'dart:math';
+import 'package:decla_time/reservations/business/reservation.dart';
+import 'package:decla_time/reservations/presentation/decleration_status_dot.dart';
+import 'package:decla_time/reservations/presentation/reservation_status_dot.dart';
+import 'package:decla_time/reservations/presentation/reservations_list/reservation_details/date_information_widget.dart';
+import 'package:flutter/material.dart';
+
+class ReservationDetailsContainer extends StatelessWidget {
+  const ReservationDetailsContainer({
+    super.key,
+    required this.reservation,
+  });
+
+  final Reservation reservation;
+  int get nights =>
+    reservation.departureDate.difference(reservation.arrivalDate).inDays + 1;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle.merge(
+      style: Theme.of(context).textTheme.bodyLarge,
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        width: 600,
+        height: min(
+          500,
+          MediaQuery.sizeOf(context).height - 250,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Wrap(
+                        //Platform and ID
+                        spacing: 24,
+                        alignment: WrapAlignment.spaceAround,
+                        children: [
+                          RichText(
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            text: TextSpan(
+                              text: "Platform: ",
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              children: [
+                                TextSpan(
+                                  text: reservation.bookingPlatform,
+                                  style:
+                                      Theme.of(context).textTheme.headlineSmall,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text("ID: ${reservation.id}"),
+                        ],
+                      ),
+                      const SizedBox.square( dimension: 8 ),
+                      Text(reservation.listingName != null
+                          ? "At ${reservation.listingName}"
+                          : ''),
+                      const SizedBox.square( dimension: 8 ),
+                      SizedBox(
+                        width: 300,
+                        child: Text(
+                          "Guest Name: ${reservation.guestName}",
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "${reservation.payout} €",
+                        style: Theme.of(context).textTheme.headlineMedium?.copyWith( color: Colors.green.shade300 ),
+                        maxLines: 1,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                      ),
+                      Text(
+                        "${ ( reservation.payout/nights ).toStringAsFixed( 2 ) } € / night",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        maxLines: 1,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                      ),
+                    ],
+                  ),
+                  DateInformationWidget(
+                    reservation: reservation,
+                    nights: nights,
+                  ),
+                ],
+              ),
+              Positioned(
+                right: -4,
+                top: -4,
+                child: ReservationStatusDot(
+                  size: 24,
+                  reservationStatusString: reservation.reservationStatus,
+                ),
+              ),
+              Positioned(
+                right: -4,
+                bottom: -4,
+                child: DeclarationStatusDot(
+                  size: 32,
+                  isDeclared: reservation.isDeclared,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
