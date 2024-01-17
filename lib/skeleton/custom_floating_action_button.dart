2f084852cc6e3bad61c874244a21cb1aebc7c846
deadlyunicorn <petrache.dev@gmail.com>
2024-01-17 19:26:10 +0200

    [TEMP] floating button: tapping loads the db with entries [temporary]!!

--- lib/skeleton/custom_floating_action_button.dart
+++ lib/skeleton/custom_floating_action_button.dart
@@ -1,11 +1,12 @@
+import 'package:decla_time/core/connection/isar_helper.dart';
 import 'package:decla_time/core/enums/selected_page.dart';
 import 'package:decla_time/core/extensions/capitalize.dart';
+import 'package:decla_time/reservations/business/reservation_actions.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
-
   const CustomFloatingActionButton({
     super.key,
     required this.selectedPage,
@@ -15,35 +16,40 @@ class CustomFloatingActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final localized = AppLocalizations.of(context)!;
 
-
-    switch( selectedPage ){
+    switch (selectedPage) {
       case SelectedPage.reservations:
       case SelectedPage.declarations:
         return AnimatedContainer(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular( 16 ),
+            borderRadius: BorderRadius.circular(16),
             color: Theme.of(context).colorScheme.surface,
           ),
           height: 48,
           width: 48,
-          duration: const Duration( milliseconds: 50),
+          duration: const Duration(milliseconds: 50),
           child: FloatingActionButton(
-            onPressed: (){
-              if ( selectedPage == SelectedPage.reservations ){
+            onPressed: () async {
+              if (selectedPage == SelectedPage.reservations) {
+                final reservations = await ReservationFolderActions
+                    .generateReservationTableFromFile("booking_gr_3.csv");
+                if ( context.mounted ){
+                  context
+                    .read<IsarHelper>()
+                    .insertMultipleEntriesToDb(reservations);
+                }
+                
                 throw UnimplementedError();
                 //Add a new reservation entry to the local database
-              }
-              else{
+              } else {
                 throw UnimplementedError();
                 //Create a new declaration to the external .gov
               }
             },
-            tooltip: selectedPage == SelectedPage.reservations 
-              ? localized.addEntries.capitalized
-              : localized.newDeclaration.capitalized,
+            tooltip: selectedPage == SelectedPage.reservations
+                ? localized.addEntries.capitalized
+                : localized.newDeclaration.capitalized,
             child: const Icon(Icons.add),
           ),
         );
@@ -51,17 +57,14 @@ class CustomFloatingActionButton extends StatelessWidget {
       case SelectedPage.analytics:
         return AnimatedContainer(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular( 8 ),
+            borderRadius: BorderRadius.circular(8),
             color: Theme.of(context).colorScheme.surface,
           ),
           height: 16,
           width: 16,
-          duration: const Duration( milliseconds: 50),
+          duration: const Duration(milliseconds: 50),
           child: const SizedBox.shrink(),
         );
-    } 
-    
+    }
   }
 }
-
-
