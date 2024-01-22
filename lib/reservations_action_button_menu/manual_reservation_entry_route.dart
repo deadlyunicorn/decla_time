3ac659739a40db0af3cdc6d1eb4a0c47f63f1777
deadlyunicorn <petrache.dev@gmail.com>
2024-01-22 22:24:07 +0200

    The 2 previous changed ( moved to file ) + Some unfinished form skeleton

--- lib/reservations_action_button_menu/manual_reservation_entry_route.dart
+++ lib/reservations_action_button_menu/manual_reservation_entry_route.dart
@@ -4,7 +4,10 @@ import 'package:decla_time/core/constants/constants.dart';
 import 'package:decla_time/core/extensions/capitalize.dart';
 import 'package:decla_time/core/widgets/route_outline.dart';
 import 'package:decla_time/reservations/business/reservation.dart';
+import 'package:decla_time/reservations_action_button_menu/reservation_manual_entry_field.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -23,7 +26,14 @@ class ManualReservationEntryRoute extends StatefulWidget {
 
 class _ManualReservationEntryRouteState
     extends State<ManualReservationEntryRoute> {
-  TextEditingController textEditingController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  var platformNameController = TextEditingController();
+  var listingNameController = TextEditingController();
+  var guestNameController = TextEditingController();
+  DateTime? arrivalDate;
+  DateTime? departureDate;
+  var payoutController = TextEditingController();
+  var reservationStatusController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,181 +41,148 @@ class _ManualReservationEntryRouteState
 
     return RouteOutline(
       title: "Προσθήκη Κράτησης",
-      child: Column(
-        children: [
-          Text("booking Platform"),
-          FutureBuilder(
-            future: getBookingPlatforms(),
-            builder: (context, snapshot) {
-              final List<String> dropdownMenuEntries = [
-                ...(snapshot.data ?? []),
-                localized.addNewPlatform.capitalized
-              ];
-
-              return DropdownMenu(
-                controller: textEditingController,
-                dropdownMenuEntries: [
-                  ...dropdownMenuEntries.map(
-                    (entryName) => DropdownMenuEntry(
-                      value: entryName,
-                      label: entryName,
-                    ),
-                  )
-                ],
-                onSelected: (value) {
-                  if (value?.toLowerCase() == localized.addNewPlatform) {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return PlatformAdditionAlertDialog(
-                            refreshParent: () {
-                              setState(() {});
-                            },
-                            dropdownMenuEntries: dropdownMenuEntries,
-                          );
-                        });
-                  }
-                },
-              );
-            },
-          ),
-          Text("Listing Name"),
-          Text("ID"),
-          Text("Guest Name"),
-          Text("ArrivalDate"),
-          Text("DepartureDate"),
-          Text("Payout"),
-          Text("Reservation Status"),
-          TextButton(
-              onPressed: () {
-                widget.addToReservationsFoundSoFar(
-                  [
-                    Reservation(
-                      bookingPlatform: "Manuual",
-                      listingName: "Custom to be edited",
-                      id: "ID",
-                      guestName: "Someone",
-                      arrivalDate: DateTime.now(),
-                      departureDate: DateTime.now(),
-                      payout: 121,
-                      reservationStatus: "Έκλεισε",
-                    )
-                  ],
-                );
-              },
-              child: Text("press mee")),
-          Text("heehe"),
-        ],
-      ),
-    );
-  }
-
-  Future<List<String>> getBookingPlatforms() async {
-    final prefs = await SharedPreferences.getInstance();
-
-    final bookingPlatforms = prefs.getStringList(kBookingPlatforms);
-    if (bookingPlatforms != null) {
-      return bookingPlatforms;
-    } else {
-      prefs.setStringList(kBookingPlatforms, ["Airbnb", "Booking.com"]);
-      return ["Airbnb", "Booking.com"];
-    }
-  }
-}
-
-class PlatformAdditionAlertDialog extends StatefulWidget {
-  const PlatformAdditionAlertDialog({
-    super.key,
-    required this.refreshParent,
-    required this.dropdownMenuEntries,
-  });
-
-  final void Function() refreshParent;
-  final List<String> dropdownMenuEntries;
-
-  @override
-  State<PlatformAdditionAlertDialog> createState() =>
-      _PlatformAdditionAlertDialogState();
-}
-
-class _PlatformAdditionAlertDialogState
-    extends State<PlatformAdditionAlertDialog> {
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController bookingPlatformNameController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    final localized = AppLocalizations.of(context)!;
-
-    return CustomAlertDialog(
-      title: localized.platformAddition.capitalized,
-      confirmButtonAction: () async {
-        if (_formKey.currentState!.validate()) {
-          Navigator.pop(context);
-
-          SharedPreferences.getInstance().then((prefs) {
-            prefs.setStringList(kBookingPlatforms,
-                [...widget.dropdownMenuEntries, bookingPlatformNameController.text]);
-            widget.refreshParent();
-          });
-        }
-      },
       child: Form(
         key: _formKey,
-        child: TextFormField(
-          decoration: InputDecoration(
-              hintText: localized.platformAdditionHint.capitalized),
-          controller: bookingPlatformNameController,
-          validator: (value) {
-            if (value!.length < 6) {
-              return localized.enterMin6Chars.capitalized;
-            } else if (widget.dropdownMenuEntries.contains(value)) {
-              return localized.bookingPlatformAlreadyExists.capitalized;
-            }
-            return null;
-          },
+        child: Column(
+          children: [
+            ReservationManualEntryField(
+              localized: localized,
+              textEditingController: platformNameController,
+              sharedPrefsListKey: kBookingPlatforms,
+              headlineText: "Platform",
+              hintText: "Platform",
+              defaultDropdownEntries: ["Airbnb", "Booking.com"]
+                  .map((platformEntry) => DropdownMenuEntry(
+                      value: platformEntry, label: platformEntry))
+                  .toList(),
+            ),
+            SizedBox.square(
+              dimension: 16,
+            ),
+            ReservationManualEntryField(
+              localized: localized,
+              textEditingController: listingNameController,
+              sharedPrefsListKey: kReservationListing,
+              headlineText: "Listing name",
+              hintText: "Listing",
+            ),
+            Text(
+              "ID",
+            ),
+            Text(
+              "Guest Name",
+            ),
+            Text("Payout"),
+            SizedBox(
+              width: kMaxContainerWidthSmall,
+              child: TextFormField(
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp("[0-9.,]"))
+                ],
+                controller: payoutController,
+                decoration: InputDecoration(
+                  errorMaxLines: 2,
+                ),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (RegExp("[0-9]+[.,][0-9][0-9]\$").hasMatch(value ?? "")) {
+                    return null;
+                  } else {
+                    return "Invalid Format. Please make sure your Format is like so: 457.00";
+                  }
+                },
+              ),
+            ),
+            Row(
+              children: [
+                Text("hello"),
+                TextButton(
+                  onPressed: () async {
+                    final arrivalDateTemp = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime(1999),
+                      lastDate:
+                          departureDate ?? DateTime(DateTime.now().year + 100),
+                      initialDate:
+                          arrivalDate ?? (departureDate ?? DateTime.now()),
+                      currentDate: DateTime.now(),
+                    );
+                    setState(() {
+                      arrivalDate = arrivalDateTemp?.add(Duration(hours: 13));
+                    });
+                  },
+                  child: Text("Arrival Date"),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final departureDateTemp = await showDatePicker(
+                      context: context,
+                      firstDate: arrivalDate ?? DateTime(1999),
+                      lastDate: DateTime(DateTime.now().year + 100),
+                      initialDate:
+                          departureDate ?? (arrivalDate ?? DateTime.now()),
+                      currentDate: DateTime.now(),
+                    );
+                    setState(() {
+                      departureDate =
+                          departureDateTemp?.add(Duration(hours: 11));
+                    });
+                  },
+                  child: Text("Departure Date"),
+                )
+              ],
+            ),
+            Text(
+              (departureDate != null && arrivalDate != null)
+                  ? "The reservation took ${departureDate?.difference(arrivalDate!).inDays} nights"
+                  : "No info",
+            ),
+            ReservationManualEntryField(
+              localized: localized,
+              textEditingController: reservationStatusController,
+              sharedPrefsListKey: kReservationStatus,
+              headlineText: "Status",
+              hintText: "Status",
+              defaultDropdownEntries: [
+                DropdownMenuEntry(
+                  value: kCompleted,
+                  label: localized.completed.capitalized,
+                ),
+                DropdownMenuEntry(
+                  value: kCancelled,
+                  label: localized.cancelled.capitalized,
+                ),
+                DropdownMenuEntry(
+                  value: kUpcoming,
+                  label: localized.upcoming.capitalized,
+                ),
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                // widget.addToReservationsFoundSoFar(
+                //   [
+                //     Reservation(
+                //       bookingPlatform: "Manuual",
+                //       listingName: "Custom to be edited",
+                //       id: "ID",
+                //       guestName: "Someone",
+                //       arrivalDate: DateTime.now(),
+                //       departureDate: DateTime.now(),
+                //       payout: 121,
+                //       reservationStatus: "Έκλεισε",
+                //     )
+                //   ],
+                // );
+                _formKey.currentState!.validate();
+              },
+              child: Text("press mee"),
+            ),
+            Text("heehe"),
+          ],
         ),
-        //( textController.text.length > 5 ),
       ),
-    );
-  }
-}
-
-class CustomAlertDialog extends StatelessWidget {
-  const CustomAlertDialog({
-    super.key,
-    required this.confirmButtonAction,
-    required this.title,
-    required this.child,
-  });
-
-  final void Function() confirmButtonAction;
-  final String title;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    final localized = AppLocalizations.of(context)!;
-
-    return AlertDialog(
-      title: Text(title),
-      content: SizedBox(
-        width: kMaxWidthSmall,
-        child: child,
-      ),
-      actions: [
-        TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(localized.cancel.capitalized)),
-        TextButton(
-          onPressed: () {
-            confirmButtonAction();
-          },
-          child: Text(localized.confirm.capitalized),
-        )
-      ],
     );
   }
 }
