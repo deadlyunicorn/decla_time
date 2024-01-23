2fe8a8338b33190f275b24efd2229bcfb5013f3e
deadlyunicorn <petrache.dev@gmail.com>
2024-01-23 17:08:11 +0200

    added hover handler and some ui adjustments

--- lib/reservations_action_button_menu/manual_entry_route/form_fields/date_fields/date_field_wrap.dart
+++ lib/reservations_action_button_menu/manual_entry_route/form_fields/date_fields/date_field_wrap.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
-class DateFieldWrap extends StatelessWidget {
-
+class DateFieldWrap extends StatefulWidget {
   const DateFieldWrap({
     super.key,
     required this.title,
     required this.handleDateSetButton,
-    required this.localized, this.date,
+    required this.localized,
+    this.date,
   });
 
   final void Function(BuildContext context) handleDateSetButton;
@@ -18,35 +18,72 @@ class DateFieldWrap extends StatelessWidget {
   final String title;
 
   @override
+  State<DateFieldWrap> createState() => _DateFieldWrapState();
+}
+
+class _DateFieldWrapState extends State<DateFieldWrap> {
+  bool isHovering = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Wrap(
-      alignment: WrapAlignment.center,
-      crossAxisAlignment: WrapCrossAlignment.center,
-      children: [
-        Text("$title: "),
-        TextButton(
-          style: datePickerTextButtonStyle(context),
-          onPressed: () {
-            handleDateSetButton(context);
-          },
-          child: Text(
-              showDateOrSet(date, false),
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          border: Border.symmetric(
+              horizontal: BorderSide(
+                  color: Theme.of(context).colorScheme.onBackground))),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        runAlignment: WrapAlignment.start,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          SizedBox(
+            width: 160,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                widget.title,
+                textAlign: TextAlign.center,
               ),
-        )
-      ],
+            ),
+          ),
+          SizedBox(
+            width: 160,
+            height: 36,
+            child: TextButton(
+              style: datePickerTextButtonStyle(context),
+              onPressed: () {
+                widget.handleDateSetButton(context);
+              },
+              onHover: (value) {
+                setState(() {
+                  isHovering = value;
+                });
+              },
+              child: SizedBox(
+                child: Text(
+                  showDateOrSet(widget.date, isHovering),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
   ButtonStyle datePickerTextButtonStyle(BuildContext context) {
     return TextButton.styleFrom(
-        textStyle: TextStyle(
-            fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
-            fontFamily: Theme.of(context).textTheme.headlineSmall?.fontFamily));
+      textStyle: TextStyle(
+        fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
+        fontFamily: Theme.of(context).textTheme.headlineSmall?.fontFamily,
+      ),
+    );
   }
 
-  String showDateOrSet(DateTime? date, bool isNotHovering) {
-    return (!isNotHovering && date != null)
+  String showDateOrSet(DateTime? date, bool isHovering) {
+    return (!isHovering && date != null)
         ? DateFormat('dd/MM/y').format(date)
-        : localized.setText.capitalized;
+        : widget.localized.setText.capitalized;
   }
 }
