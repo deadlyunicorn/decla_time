7434857e1e285021fb46005aae9092be795256e0
deadlyunicorn <petrache.dev@gmail.com>
2024-01-23 13:19:43 +0200

    added a "clear all" button to clear all databases. ONLY FOR TESTING.

--- lib/analytics/analytics_page.dart
+++ lib/analytics/analytics_page.dart
@@ -1,14 +1,18 @@
+import 'package:decla_time/core/connection/isar_helper.dart';
+import 'package:decla_time/core/constants/constants.dart';
+import 'package:decla_time/core/extensions/text_button_error.dart';
 import 'package:decla_time/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:isar/isar.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AnalyticsPage extends StatelessWidget {
   const AnalyticsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     final localized = AppLocalizations.of(context)!;
 
     return Column(
@@ -16,13 +20,10 @@ class AnalyticsPage extends StatelessWidget {
         const Expanded(child: Text("Analytics here")),
         Center(
           child: TextButton(
-            
             style: TextButton.styleFrom(
-              foregroundColor: Theme.of(context).colorScheme.surface,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular( 4 )
-              )
-            ),
+                foregroundColor: Theme.of(context).colorScheme.surface,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4))),
             onPressed: () {
               context.read<SettingsController>().toggleLocale();
             },
@@ -31,6 +32,29 @@ class AnalyticsPage extends StatelessWidget {
             ),
           ),
         ),
+        Center(
+          child: TextButton(
+            style: Theme.of(context).textButtonErrorTheme.style,
+            onPressed: () async {
+              final isarFuture = context.read<IsarHelper>().isarFuture;
+              final prefsFuture = SharedPreferences.getInstance();
+
+              final [ isar as Isar ,prefs as SharedPreferences ] = await Future.wait([isarFuture,prefsFuture]);
+
+              await isar.writeTxn(() async {
+                await isar.clear();
+              });
+
+              await prefs.setStringList(kListings, []);
+              await prefs.setStringList(kBookingPlatforms, []);
+              await prefs.setStringList(kReservationStatus, []);
+            },
+            child: Text(
+              "Clear everything",
+              style: TextStyle(color: Theme.of(context).colorScheme.error),
+            ),
+          ),
+        )
       ],
     );
   }
