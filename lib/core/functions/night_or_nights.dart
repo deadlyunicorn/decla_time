dd99bd9c0ba852e8043eb87a0e3a8968c9b4750a
deadlyunicorn <petrache.dev@gmail.com>
2024-01-24 14:24:38 +0200

    added a function that returns "night" or "nights" based on night input count. ?? I thought I had already created this..

---
+++ lib/core/functions/night_or_nights.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+String nightOrNights( AppLocalizations localized, int nights ){
+
+  if ( nights <= 1 ){
+    return "$nights ${localized.night}";
+  }
+  else{
+    return "$nights ${localized.nights}";
+  }
+
+}
