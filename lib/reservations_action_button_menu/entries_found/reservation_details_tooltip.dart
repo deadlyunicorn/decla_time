14a273fe6cc86649e90b5da16e41ce012120d3aa
deadlyunicorn <petrache.dev@gmail.com>
2024-02-05 21:50:24 +0200

    extracted reservation details tooltip to its own file

---
+++ lib/reservations_action_button_menu/entries_found/reservation_details_tooltip.dart
@@ -0,0 +1,66 @@
+import 'package:decla_time/core/extensions/capitalize.dart';
+import 'package:decla_time/reservations/business/reservation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ReservationDetailsTooltip extends StatelessWidget {
+  const ReservationDetailsTooltip({
+    super.key,
+    required this.reservation,
+    required this.localized,
+  });
+
+  final Reservation reservation;
+  final AppLocalizations localized;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      top: 0,
+      child: Tooltip(
+        preferBelow: false,
+        richMessage: TextSpan(children: [
+          TextSpan(
+            text: "ID: ${reservation.id}",
+          ),
+          const TextSpan(text: "\n"),
+          TextSpan(
+            text:
+                "${localized.listingPlace.capitalized}: ${reservation.listingName ?? localized.without.capitalized}",
+          ),
+          const TextSpan(text: "\n"),
+          TextSpan(
+            text:
+                "${localized.platform.capitalized}: ${reservation.bookingPlatform}",
+          ),
+          const TextSpan(text: "\n"),
+          TextSpan(
+            text:
+                "${localized.status.capitalized}: ${reservation.reservationStatus}",
+          ),
+          const TextSpan(text: "\n"),
+          TextSpan(
+            text:
+                "${localized.arrival.capitalized}: ${reservation.arrivalDateString}",
+          ),
+          const TextSpan(text: "\n"),
+          TextSpan(
+            text:
+                "${localized.departure.capitalized}: ${reservation.departureDateString}",
+          ),
+          const TextSpan(text: "\n"),
+          TextSpan(
+            text:
+                "${localized.payout.capitalized}: ${reservation.payout}€",
+          ),
+        ]),
+        child: Icon(
+          Icons.info,
+          color:
+              Theme.of(context).colorScheme.surface,
+        ),
+      ),
+    );
+  }
+}
