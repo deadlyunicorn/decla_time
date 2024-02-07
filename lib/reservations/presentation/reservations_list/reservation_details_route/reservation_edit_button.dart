8e36dfa3035dd2dc6f3dfd596e00ca5715de65d2
deadlyunicorn <petrache.dev@gmail.com>
2024-02-07 23:00:13 +0200

    Added a ToolTip surrounding the ReservationEditButton

--- lib/reservations/presentation/reservations_list/reservation_details_route/reservation_edit_button.dart
+++ lib/reservations/presentation/reservations_list/reservation_details_route/reservation_edit_button.dart
@@ -1,39 +1,46 @@
+import 'package:decla_time/core/extensions/capitalize.dart';
 import 'package:decla_time/reservations/business/reservation.dart';
-import 'package:decla_time/reservations/presentation/reservations_list/reservation_details_route/reservation_editing_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:decla_time/reservations/presentation/reservations_list/reservation_details_route/reservation_editing_route.dart';
 
 class ReservationEditButton extends StatelessWidget {
   const ReservationEditButton({
     super.key,
     required this.reservation,
     required this.size,
+    required this.localized,
   });
 
   final Reservation reservation;
   final double size;
+  final AppLocalizations localized;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: size,
-      width: size,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return ReservationEditingRoute(
-                  reservation: reservation,
-                );
-              },
-            ),
-          );
-        },
-        child: const Icon(
-          Icons.edit,
-          size: 24,
-          color: Colors.amber,
+    return Tooltip(
+      message: localized.reservationEntryChange.capitalized,
+      child: SizedBox(
+        height: size,
+        width: size,
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return ReservationEditingRoute(
+                    reservation: reservation,
+                  );
+                },
+              ),
+            );
+          },
+          child: const Icon(
+            Icons.edit,
+            size: 24,
+            color: Colors.amber,
+          ),
         ),
       ),
     );
