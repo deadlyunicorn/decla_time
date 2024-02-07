01050d8126fe7976c78db20e1e02004e1658d447
deadlyunicorn <petrache.dev@gmail.com>
2024-02-07 18:19:07 +0200

    Created a "Reservation Form" by taking apart the Manual Reservation Entry Route + added optional initial values. also changed the alignments etc.

---
+++ lib/reservations/presentation/widgets/reservation_form.dart
@@ -0,0 +1,263 @@
+import 'package:decla_time/core/connection/isar_helper.dart';
+import 'package:decla_time/core/constants/constants.dart';
+import 'package:decla_time/core/extensions/capitalize.dart';
+import 'package:decla_time/core/functions/fasthash.dart';
+import 'package:decla_time/core/functions/translate_reservation_status.dart';
+import 'package:decla_time/reservations/business/reservation.dart';
+import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/date_fields/date_pickers_field.dart';
+import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/listing_name_field.dart';
+import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/payout_field.dart';
+import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/platform_field.dart';
+import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/required_text_field.dart';
+import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/status_field.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ReservationForm extends StatefulWidget {
+  const ReservationForm({
+    super.key,
+    required this.localized,
+    required this.handleFormSubmit,
+    this.platformName,
+    this.listingName,
+    this.id,
+    this.guestName,
+    this.payout,
+    this.reservationStatus,
+    this.arrivalDate,
+    this.departureDate,
+  });
+
+  final AppLocalizations localized;
+  final void Function(Reservation reservation) handleFormSubmit;
+
+  //? Below are default values -
+  final String? platformName;
+  final String? listingName;
+  final String? id;
+  final String? guestName;
+  final String? payout;
+  final String? reservationStatus;
+  final DateTime? arrivalDate;
+  final DateTime? departureDate;
+
+  @override
+  State<ReservationForm> createState() => _ReservationFormState();
+}
+
+class _ReservationFormState extends State<ReservationForm> {
+  final _formKey = GlobalKey<FormState>();
+  final platformNameController = TextEditingController();
+  final listingNameController = TextEditingController();
+  final idController = TextEditingController();
+  final guestNameController = TextEditingController();
+  final payoutController = TextEditingController();
+  final reservationStatusController = TextEditingController();
+  DateTime? arrivalDate;
+  DateTime? departureDate;
+
+  @override
+  void initState() {
+    super.initState();
+
+    platformNameController.text = widget.platformName ?? "";
+    listingNameController.text = widget.listingName ?? "";
+    idController.text = widget.id ?? "";
+    guestNameController.text = widget.guestName ?? "";
+    payoutController.text = widget.payout ?? "";
+    reservationStatusController.text = widget.reservationStatus != null
+        ? translateReservationStatus(widget.reservationStatus ?? "").name
+        : "";
+    arrivalDate = widget.arrivalDate;
+    departureDate = widget.departureDate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            width: kMaxWidthLargest,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      PlatformField(
+                        localized: widget.localized,
+                        platformNameController: platformNameController,
+                      ),
+                      ListingNameField(
+                        localized: widget.localized,
+                        listingNameController: listingNameController,
+                      ),
+                      StatusField(
+                        localized: widget.localized,
+                        reservationStatusController:
+                            reservationStatusController,
+                      ),
+                    ],
+                  ),
+                  Wrap(
+                    runSpacing: 16,
+                    spacing: 16,
+                    children: [
+                      SizedBox(
+                        width: kMaxContainerWidthSmall * 2,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              height: 64,
+                              child: Center(
+                                child: IconButton(
+                                  onPressed: () {
+                                    if (!hasInitialId()) {
+                                      idController.text =
+                                          fastHash(DateTime.now().toString())
+                                              .abs()
+                                              .toRadixString(16)
+                                              .substring(0, 10)
+                                              .toUpperCase();
+                                    }
+                                  },
+                                  icon: const Icon(Icons.shuffle_rounded),
+                                ),
+                              ),
+                            ),
+                            const SizedBox.square(
+                              dimension: 8,
+                            ),
+                            Flexible(
+                              child: RequiredTextField(
+                                  isEditingExistingEntry: hasInitialId(),
+                                  controller: idController,
+                                  label: "ID",
+                                  localized: widget.localized,
+                                  refresh: refresh),
+                            ),
+                          ],
+                        ),
+                      ),
+                      RequiredTextField(
+                        controller: guestNameController,
+                        label: widget.localized.guestName.capitalized,
+                        localized: widget.localized,
+                      ),
+                    ],
+                  ),
+                  const SizedBox.square(
+                    dimension: 16,
+                  ),
+
+                  PayoutField(
+                    payoutController: payoutController,
+                    localized: widget.localized,
+                  ),
+                  DatePickersField(
+                    departureDate: departureDate,
+                    arrivalDate: arrivalDate,
+                    localized: widget.localized,
+                    setArrivalDate: setArrivalDate,
+                    setDepartureDate: setDapartureDate,
+                  ),
+                  FutureBuilder(
+                      future: idAlreadyExistsFuture(),
+                      builder: (context, snapshot) {
+                        final reservationAlreadyRegistered =
+                            snapshot.data ?? false;
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            reservationAlreadyRegistered
+                                ? widget
+                                    .localized
+                                    .willOverwriteExistingReservation
+                                    .capitalized
+                                : "",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(color: Colors.amber),
+                          ),
+                        );
+                      }),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          final bookingPlatform = platformNameController.text;
+                          final listingName = listingNameController.text;
+                          final id = idController.text;
+                          final guestName = guestNameController.text;
+                          final payout =
+                              double.tryParse(payoutController.text)!;
+                          final reservationStatus =
+                              reservationStatusController.text;
+                          final arrivalDate = this.arrivalDate!;
+                          final departureDate = this.departureDate!;
+
+                          final reservation = Reservation(
+                            bookingPlatform: bookingPlatform,
+                            listingName: listingName,
+                            id: id,
+                            guestName: guestName,
+                            arrivalDate: arrivalDate,
+                            departureDate: departureDate,
+                            payout: payout,
+                            reservationStatus: reservationStatus,
+                          );
+
+                          widget.handleFormSubmit(reservation);
+                          Navigator.pop(context, reservation);
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(widget.localized.submit.capitalized,
+                            style: Theme.of(context).textTheme.headlineLarge),
+                      ),
+                    ),
+                  ),
+                  // const Expanded(child: Text("heehe")),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> idAlreadyExistsFuture() async {
+    return await context.read<IsarHelper>().idAlreadyExists(idController.text);
+  }
+
+  void refresh() {
+    setState(() {});
+  }
+
+  void setArrivalDate(DateTime? newDate) {
+    setState(() {
+      arrivalDate = newDate?.add(const Duration(hours: 13));
+    });
+  }
+
+  void setDapartureDate(DateTime? newDate) {
+    setState(() {
+      departureDate = newDate?.add(const Duration(hours: 11));
+    });
+  }
+
+  bool hasInitialId() {
+    final initialId = widget.id;
+    return (initialId != null && initialId.isNotEmpty);
+  }
+}
--- lib/reservations_action_button_menu/manual_entry_route/manual_reservation_entry_route.dart
+++ lib/reservations_action_button_menu/manual_entry_route/manual_reservation_entry_route.dart
@@ -1,18 +1,11 @@
-import 'package:decla_time/core/constants/constants.dart';
 import 'package:decla_time/core/extensions/capitalize.dart';
-import 'package:decla_time/core/functions/fasthash.dart';
 import 'package:decla_time/core/widgets/route_outline.dart';
 import 'package:decla_time/reservations/business/reservation.dart';
-import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/date_fields/date_pickers_field.dart';
-import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/required_text_field.dart';
-import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/listing_name_field.dart';
-import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/payout_field.dart';
-import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/platform_field.dart';
-import 'package:decla_time/reservations_action_button_menu/manual_entry_route/form_fields/status_field.dart';
+import 'package:decla_time/reservations/presentation/widgets/reservation_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ManualReservationEntryRoute extends StatefulWidget {
+class ManualReservationEntryRoute extends StatelessWidget {
   const ManualReservationEntryRoute({
     super.key,
     required this.addToReservationsFoundSoFar,
@@ -21,167 +14,16 @@ class ManualReservationEntryRoute extends StatefulWidget {
   final void Function(Iterable<Reservation> list) addToReservationsFoundSoFar;
 
   @override
-  State<ManualReservationEntryRoute> createState() =>
-      _ManualReservationEntryRouteState();
-}
-
-class _ManualReservationEntryRouteState
-    extends State<ManualReservationEntryRoute> {
-  final _formKey = GlobalKey<FormState>();
-  final platformNameController = TextEditingController();
-  final listingNameController = TextEditingController();
-  final idController = TextEditingController();
-  final guestNameController = TextEditingController();
-  final payoutController = TextEditingController();
-  final reservationStatusController = TextEditingController();
-  DateTime? arrivalDate;
-  DateTime? departureDate;
-
-  @override
   Widget build(BuildContext context) {
     final localized = AppLocalizations.of(context)!;
 
     return RouteOutline(
-      title: localized.manualAddition.capitalized,
-      child: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.sizeOf(context).width,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SizedBox(
-              width: kMaxWidthLargest,
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Wrap(
-                      alignment: WrapAlignment.center,
-                      children: [
-                        PlatformField(
-                          localized: localized,
-                          platformNameController: platformNameController,
-                        ),
-                        ListingNameField(
-                          localized: localized,
-                          listingNameController: listingNameController,
-                        ),
-                        StatusField(
-                          localized: localized,
-                          reservationStatusController:
-                              reservationStatusController,
-                        ),
-                      ],
-                    ),
-                    Wrap(
-                      children: [
-                        SizedBox(
-                          width: kMaxContainerWidthSmall * 2,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 32),
-                                child: IconButton(
-                                  onPressed: () {
-                                    idController.text =
-                                        fastHash(DateTime.now().toString())
-                                            .abs()
-                                            .toRadixString(16)
-                                            .substring(0, 10);
-                                  },
-                                  icon: const Icon(Icons.shuffle_rounded),
-                                ),
-                              ),
-                              Flexible(
-                                child: RequiredTextField(
-                                  controller: idController,
-                                  label: "ID",
-                                  localized: localized,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        RequiredTextField(
-                          controller: guestNameController,
-                          label: localized.guestName.capitalized,
-                          localized: localized,
-                        ),
-                      ],
-                    ),
-                    PayoutField(
-                      payoutController: payoutController,
-                      localized: localized,
-                    ),
-                    DatePickersField(
-                      departureDate: departureDate,
-                      arrivalDate: arrivalDate,
-                      localized: localized,
-                      setArrivalDate: setArrivalDate,
-                      setDepartureDate: setDapartureDate,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          final bookingPlatform = platformNameController.text;
-                          final listingName = listingNameController.text;
-                          final id = idController.text;
-                          final guestName = guestNameController.text;
-                          final payout =
-                              double.tryParse(payoutController.text)!;
-                          final reservationStatus =
-                              reservationStatusController.text;
-                          final arrivalDate = this.arrivalDate!;
-                          final departureDate = this.departureDate!;
-
-                          final reservation = Reservation(
-                            bookingPlatform: bookingPlatform,
-                            listingName: listingName,
-                            id: id,
-                            guestName: guestName,
-                            arrivalDate: arrivalDate,
-                            departureDate: departureDate,
-                            payout: payout,
-                            reservationStatus: reservationStatus,
-                          );
-
-                          widget.addToReservationsFoundSoFar([reservation]);
-
-                          Navigator.pop(context, reservation);
-                        }
-                      },
-                      child: Text(
-                        localized.submit.capitalized,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineLarge
-                            ?.copyWith(
-                                color: Theme.of(context).colorScheme.surface),
-                      ),
-                    ),
-                    // const Expanded(child: Text("heehe")),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  void setArrivalDate(DateTime? newDate) {
-    setState(() {
-      arrivalDate = newDate?.add(const Duration(hours: 13));
-    });
-  }
-
-  void setDapartureDate(DateTime? newDate) {
-    setState(() {
-      departureDate = newDate?.add(const Duration(hours: 11));
-    });
+        title: localized.manualAddition.capitalized,
+        child: ReservationForm(
+          localized: localized,
+          handleFormSubmit: (Reservation reservation) {
+            addToReservationsFoundSoFar([reservation]);
+          },
+        ));
   }
 }
