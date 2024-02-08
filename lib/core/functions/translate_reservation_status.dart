7be59ab8e5429c73ba498c0985895d021b73e7bb
deadlyunicorn <petrache.dev@gmail.com>
2024-02-08 12:20:41 +0200

    changed reservationStatus translation from switch to a dictionary - now it contains localized text as well.

--- lib/core/functions/translate_reservation_status.dart
+++ lib/core/functions/translate_reservation_status.dart
@@ -1,19 +1,19 @@
 import 'package:decla_time/core/constants/constants.dart';
 import 'package:decla_time/core/enums/reservation_status.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-ReservationStatus translateReservationStatus( String reservationStatusString ){
-    switch( reservationStatusString.toLowerCase() ){
-
-      case "ok":
-      case kCompleted:
-      case "past guest":
-      case "awaiting guest review":
-        return ReservationStatus.completed;
-      case kCancelled:
-      case "cancelled_by_guest":
-        return ReservationStatus.cancelled;
-      default:
-        return ReservationStatus.upcoming;
-
-    }
-  }
+ReservationStatus translateReservationStatus(
+    String reservationStatusString, AppLocalizations localized) {
+  final Map<String, ReservationStatus> reservationStatusSet = {
+    "ok": ReservationStatus.completed,
+    kCompleted: ReservationStatus.completed,
+    "past guest": ReservationStatus.completed,
+    localized.completed: ReservationStatus.completed,
+    "awaiting guest review": ReservationStatus.completed,
+    kCancelled: ReservationStatus.cancelled,
+    localized.cancelled: ReservationStatus.cancelled,
+    "cancelled_by_guest": ReservationStatus.cancelled,
+  };
+  return reservationStatusSet[reservationStatusString.toLowerCase()] ??
+      ReservationStatus.upcoming;
+}
