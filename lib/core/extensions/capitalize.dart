b7920fd1483313db199bb0e3059d6e1ea9f40cd6
deadlyunicorn <petrache.dev@gmail.com>
2024-02-08 12:49:56 +0200

    logic fixes to the capitalized getter

--- lib/core/extensions/capitalize.dart
+++ lib/core/extensions/capitalize.dart
@@ -1,4 +1,8 @@
 extension CapitalizeString on String{
 
-  String get capitalized  => "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
+  String get capitalized  => length > 1 
+    ? "${this[0].toUpperCase()}${substring(1).toLowerCase()}"
+    : length == 1 
+      ? this[0].capitalized
+      : "";
 }
