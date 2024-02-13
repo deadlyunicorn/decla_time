450ae96588cf4c6ccb18d4bb6f33cdbc68f06892
deadlyunicorn <petrache.dev@gmail.com>
2024-02-13 10:24:12 +0200

    added getting headers from file for testing with sample files.

--- tests/declaration_tests/test_headers.dart
+++ tests/declaration_tests/test_headers.dart
@@ -1,19 +1,43 @@
-import 'package:decla_time/declarations/login/declarations_page_headers.dart';
+// ignore_for_file: non_constant_identifier_names
 
-DeclarationsPageHeaders getTestHeaders(){
+import 'dart:io';
+
+import 'package:decla_time/declarations/login/declarations_page_headers.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'get_values_from_html.dart';
+
+Future<DeclarationsPageHeaders> getTestHeadersFromFileFuture(File file) async {
+  final fileContent = await file.readAsString();
+
+  final gsisCookie = getValuesFromHtml(fileContent, ">gsisCookie:", ";")[0];
+  final wl_authCookie_jSessionId =
+      getValuesFromHtml(fileContent, ">wl_authCookie_jSessionId:", ";")[0];
+  final jSessionId = getValuesFromHtml(fileContent, ">jSessionId:", ";")[0];
+  final oamAuthnCookie =
+      getValuesFromHtml(fileContent, ">oamAuthnCookie:", ";")[0];
 
   final testingHeaders = DeclarationsPageHeaders(
-      gsisCookie:
-          "<get from file>",
-      wl_authCookie_jSessionId: "<get from file>",
-      jSessionId:
-          "<get from file>",
-    );
+    gsisCookie: gsisCookie,
+    wl_authCookie_jSessionId: wl_authCookie_jSessionId,
+    jSessionId: jSessionId,
+  );
 
-    testingHeaders.newSession(
-      oamAuthnCookie:
-          "<get from file>",
-    );
+  testingHeaders.newSession(
+    oamAuthnCookie: oamAuthnCookie,
+  );
 
-    return testingHeaders;
+  return testingHeaders;
+}
+
+void main() {
+  test("getting headers from file", () async {
+    final sampleFile = File("headersSample.txt");
+    final headers = await getTestHeadersFromFileFuture(sampleFile);
+
+    expect(headers.gsisCookie, "thisIsGsis");
+    expect(headers.oamAuthnCookie, "thisIsOam");
+    expect(headers.jSessionId, "thisIsJsessionId");
+    expect(headers.wl_authCookie_jSessionId, "ThisIsWl_auth");
+  });
 }
