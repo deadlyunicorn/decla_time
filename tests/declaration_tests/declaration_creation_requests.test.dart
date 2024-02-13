8dc9be67c5cace4ad56afc3fa75b34b541d7c963
deadlyunicorn <petrache.dev@gmail.com>
2024-02-13 09:55:17 +0200

    unifinished test that creates a new declaration ( might not be needed in the end )

---
+++ tests/declaration_tests/declaration_creation_requests.test.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: prefer_const_declarations, non_constant_identifier_names, avoid_print
+
+import 'package:decla_time/declarations/login/declaration_body.dart';
+import 'package:decla_time/declarations/login/declarations_page_headers.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+import 'test_headers.dart';
+
+void main() async {
+  //! Step 1: Get to the Declaration Form. Get a valid view_state from it.
+
+  test("Test that creates a new declaration", () async {
+    final testingHeaders = getTestHeaders();
+    // final res1 = await createNewDeclarationRequest(
+    //   headersObject: testingHeaders,
+    //   newDeclarationBodyString: DeclarationBody.declarationCreationBody,
+    // );
+    final res1 = await createNewDeclarationRequest(
+      headersObject: testingHeaders,
+      newDeclarationBodyString: DeclarationBody(
+              arrivalDate: DateTime.now(),
+              departureDate: DateTime.now().add(const Duration(days: 3)),
+              payout: 0.08,
+              platform: "Airbnb",
+              viewState: (() {
+                return null;
+              })())
+          .bodyString,
+    );
+
+    print(res1.body); //!! returns a ViewState.
+    print(res1.statusCode);
+
+    expect(res1.statusCode, 200); //TODO make an actual test.
+  });
+}
+
+Future<Response> createNewDeclarationRequest({
+  required DeclarationsPageHeaders headersObject,
+  required String newDeclarationBodyString,
+}) async {
+  return http.post(
+    Uri.https(
+        "www1.aade.gr", "taxisnet/short_term_letting/views/declaration.xhtml"),
+    headers: headersObject.getHeaders(),
+    body: newDeclarationBodyString,
+  );
+}
