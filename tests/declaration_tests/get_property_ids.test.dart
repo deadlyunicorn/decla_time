78f1559f5d61573b3585e44a0389bb384d8f6f09
deadlyunicorn <petrache.dev@gmail.com>
2024-02-13 22:56:46 +0200

    minor refactoring

--- tests/declaration_tests/get_property_ids.test.dart
+++ tests/declaration_tests/get_property_ids.test.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: prefer_const_declarations, non_constant_identifier_names, avoid_print
 
-import 'dart:io';
-
 import 'package:decla_time/declarations/functions/check_if_logged_in.dart';
 import 'package:decla_time/declarations/http_requests/get_user_properties.dart';
+import 'package:decla_time/declarations/login/declarations_page_headers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'test_headers.dart';
@@ -11,17 +10,9 @@ import 'user_property_information.dart';
 
 void main() async {
   test("Test that gets a list of the available property Ids", () async {
-    final testingHeaders = await getTestHeaders(); 
+    final testingHeaders = await getTestHeaders();
 
-    final res = await getPropertiesRequest(
-      headersObject: testingHeaders,
-    );
-
-    final body = res.body;
-    checkIfLoggedIn(body);
-
-    final userProperties = UserPropertiesInformation.generateFromHtml(body);
-
+    final userProperties = await getUserProperties(testingHeaders);
     if (userProperties.propertyIds.isEmpty) {
       throw "No properties found";
     } else {
@@ -33,8 +24,6 @@ void main() async {
     print(userProperties.addressOfProperties);
     print(userProperties.registryNumbers);
 
-    print(res.statusCode);
-
     expect(
         userProperties.atakNumbers.length, userProperties.propertyIds.length);
     expect(userProperties.addressOfProperties.length,
@@ -42,4 +31,18 @@ void main() async {
     expect(userProperties.registryNumbers.length,
         userProperties.propertyIds.length);
   });
+}
+
+Future<UserPropertiesInformation> getUserProperties(
+  DeclarationsPageHeaders headers,
+) async {
+  final res = await getPropertiesRequest(
+    headersObject: headers,
+  );
+
+  final body = res.body;
+  print(res.statusCode);
+  checkIfLoggedIn(body); //! Can throw error
+
+  return UserPropertiesInformation.generateFromHtml(body);
 }
