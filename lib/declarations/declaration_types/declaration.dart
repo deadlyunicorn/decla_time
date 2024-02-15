83cb08d1bdca70d8f51f52f0ae1fda9faafe2c6e
deadlyunicorn <petrache.dev@gmail.com>
2024-02-15 20:41:34 +0200

    added a Declaration Class and a FinalizedDeclarationDetails Class ( issues with isar :D )

---
+++ lib/declarations/declaration_types/declaration.dart
@@ -0,0 +1,61 @@
+import 'package:decla_time/core/enums/booking_platform.dart';
+import 'package:decla_time/core/enums/declaration_status.dart';
+import 'package:decla_time/core/functions/fasthash.dart';
+import 'package:intl/intl.dart';
+import 'package:isar/isar.dart';
+
+part 'declaration.g.dart';
+
+@collection
+class Declaration {
+  @Enumerated(EnumType.name)
+  final BookingPlatform bookingPlatform;
+
+  final String propertyId;
+
+  @Enumerated(EnumType.name)
+  final DeclarationStatus declarationStatus;
+
+  final DateTime arrivalDate;
+  final DateTime departureDate;
+  final double payout;
+
+  final DateTime? cancellationDate;
+  final double? cancellationAmount;
+
+  @Index(unique: true)
+  final int declarationDbId;
+
+  @Index(unique: true)
+  int? serialNumber; //used for getting finalized declaration details.
+
+  Declaration(
+      {required this.propertyId,
+      required this.declarationDbId,
+      this.declarationStatus = DeclarationStatus.temporary,
+      this.cancellationDate,
+      this.cancellationAmount,
+      required this.bookingPlatform,
+      required this.arrivalDate,
+      required this.departureDate,
+      required this.payout,
+      this.serialNumber});
+
+  int get nights => departureDate.difference(arrivalDate).inDays + 1;
+  String get arrivalDateString => DateFormat("dd/MM/y").format(arrivalDate);
+  String get departureDateString => DateFormat("dd/MM/y").format(departureDate);
+  Id get isarId => fastHash("$declarationDbId");
+
+  bool isEqualTo(Declaration declaration) {
+    return declaration.propertyId == propertyId &&
+        declaration.declarationDbId == declarationDbId &&
+        declaration.declarationStatus == declarationStatus &&
+        declaration.cancellationDate == cancellationDate &&
+        declaration.cancellationAmount == cancellationAmount &&
+        declaration.bookingPlatform == bookingPlatform &&
+        declaration.arrivalDate == arrivalDate &&
+        declaration.departureDate == departureDate &&
+        declaration.payout == payout &&
+        declaration.serialNumber == serialNumber;
+  }
+}
---
+++ lib/declarations/declaration_types/finalized_declaration_details.dart
@@ -0,0 +1,40 @@
+import 'package:decla_time/core/enums/declaration_type.dart';
+import 'package:decla_time/core/functions/fasthash.dart';
+import 'package:isar/isar.dart';
+
+
+part 'finalized_declaration_details.g.dart';
+
+
+@collection
+class FinalizedDeclarationDetails{
+  final DateTime declarationDate;
+
+  @Enumerated(EnumType.name)
+  final DeclarationType declarationType; //Ammending or Initial
+
+  @Index(unique: true)
+  final int serialNumber;
+
+  DateTime?
+      amendmentDate; //! We basically check which declarations have not been amended. Don't display a reservation that has been amandeded.
+  final List<int> relatedDeclarationsSerialNumbers =
+      []; //! We have a "history" button for the declaration. We can check our ammendments. - Only a finalized declaration can have a SerialNumber.
+
+  FinalizedDeclarationDetails({
+    required this.declarationDate,
+    required this.declarationType,
+    required this.serialNumber,
+    this.amendmentDate,
+  });
+
+  Id get isarId => fastHash("$serialNumber");
+
+  bool isEqualTo(FinalizedDeclarationDetails finalizedDeclarationDetails) {
+    return 
+        finalizedDeclarationDetails.declarationDate == declarationDate &&
+        finalizedDeclarationDetails.declarationType == declarationType &&
+        finalizedDeclarationDetails.serialNumber == serialNumber &&
+        finalizedDeclarationDetails.amendmentDate == amendmentDate;
+  }
+}
