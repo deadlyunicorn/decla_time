92853c30d618f3ac6b8b28ea0c441ef35cadd49c
deadlyunicorn <petrache.dev@gmail.com>
2024-02-15 15:07:07 +0200

    removed viewState from declarationBody. bodyString -> bodyStringPOST ( String viewStateParsed )

--- lib/declarations/login/declaration_body.dart
+++ lib/declarations/login/declaration_body.dart
@@ -5,7 +5,6 @@ class DeclarationBody {
   final DateTime arrivalDate;
   final DateTime departureDate;
   final double payout;
-  final String _viewState;
   final DateTime? cancellationDate;
   final double? cancellationAmount;
 
@@ -14,10 +13,9 @@ class DeclarationBody {
     required this.departureDate,
     required this.payout,
     required this.platform,
-    required String viewState,
     this.cancellationDate,
     this.cancellationAmount,
-  }): _viewState = viewState;
+  });
 
   int get _translatedPlatform {
     final platformName = platform.toLowerCase();
@@ -47,7 +45,7 @@ class DeclarationBody {
     return DateFormat("dd%2FMM%2Fy").format(date);
   }
 
-  String get bodyString {
+  String bodyStringPOST( String viewStateParsed ) {
     double? payout = this.payout;
 
     if (cancellationAmount != null) {
@@ -66,27 +64,7 @@ class DeclarationBody {
     return "appForm%3ArentalFrom_input=${translateDate(arrivalDate)}&appForm%3ArentalTo_input=${translateDate(departureDate)}&appForm%3AsumAmount_hinput=${payout ?? ""}&appForm%3Aplatform_input=$_translatedPlatform&appForm%3AcancelAmount_hinput=${cancellationAmount ?? ""}&appForm%3AcancelDate_input=${cancellationDate != null ? translateDate(cancellationDate!) : ""}&appForm%3ArenterAfm=000000000&appForm%3ApaymentType_focus=&appForm%3ApaymentType_input=4&appForm%3Aplatform_focus=&appForm%3Aj_idt93=&javax.faces.ViewState=$viewStateParsed&javax.faces.partial.ajax=true&javax.faces.source=appForm%3AsaveButton&javax.faces.partial.execute=%40all&javax.faces.partial.render=appForm&appForm%3AsaveButton=appForm%3AsaveButton&appForm=appForm";
   }
 
-  // String get submitReservationBody { //!To be removed.
-  //   double? payout = this.payout;
-
-  //   if (cancellationAmount != null) {
-  //     if (payout > 0) {
-  //       throw ("Cancellation Amount while Payout > 0");
-  //     }
-  //     if (cancellationDate == null) {
-  //       throw "Cancellation date missing";
-  //     }
-  //     payout = null;
-  //   } else if (cancellationDate != null) {
-  //     // && cancellation Amount == null
-  //     throw "Cancellation date missing";
-  //   }
-  //   return "${viewStateParsed}appForm%3ArentalFrom_input=${translateDate(arrivalDate)}&appForm%3ArentalTo_input=${translateDate(departureDate)}&appForm%3AsumAmount_hinput=${payout ?? ""}&appForm%3Aplatform_input=$_translatedPlatform&appForm%3AcancelAmount_hinput=${cancellationAmount ?? ""}&appForm%3AcancelDate_input=${cancellationDate != null ? translateDate(cancellationDate!) : ""}&appForm%3ArenterAfm=000000000&appForm%3ApaymentType_focus=&appForm%3ApaymentType_input=4&appForm%3Aplatform_focus=&appForm%3Aj_idt93=&javax.faces.partial.ajax=true&javax.faces.source=appForm%3AsaveButton&javax.faces.partial.execute=%40all&javax.faces.partial.render=appForm&appForm%3AsaveButton=appForm%3AsaveButton&appForm=appForm&";
-  // }
-
-  String get viewStateParsed => "${_viewState.split(":")[0]}%3A${_viewState.split(":")[1] }";
-
-
+ 
   static String get declarationCreationBody { 
     return "javax.faces.partial.ajax=true&javax.faces.source=appForm%3AnewDeclarationButtonLower&javax.faces.partial.execute=%40all&appForm%3AnewDeclarationButtonLower=appForm%3AnewDeclarationButtonLower&appForm=appForm";
   }
