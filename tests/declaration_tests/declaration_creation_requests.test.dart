2ce0e68914ff52e13d520deff2b98754c599c642
deadlyunicorn <petrache.dev@gmail.com>
2024-02-15 11:21:52 +0200

    finished the test for creating a new declaration

--- tests/declaration_tests/declaration_creation_requests.test.dart
+++ tests/declaration_tests/declaration_creation_requests.test.dart
@@ -1,52 +1,76 @@
 // ignore_for_file: prefer_const_declarations, non_constant_identifier_names, avoid_print
 
-
 import 'package:decla_time/declarations/login/declaration_body.dart';
 import 'package:decla_time/declarations/login/declarations_page_headers.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
+import 'get_values_between_strings.dart';
 import 'values.dart';
 
-//TODO Finish this test.
 void main() async {
-  //! Step 1: Get to the Declaration Form. Get a valid view_state from it.
-
   test("Test that creates a new declaration", () async {
     final testingHeaders = getTestHeaders();
+    final samplePropertyId = propertyId;
 
-    // final res1 = await createNewDeclarationRequest(
-    //   headersObject: testingHeaders,
-    //   newDeclarationBodyString: DeclarationBody.declarationCreationBody,
-    // );
-    final res1 = await createNewDeclarationRequest(
-      headersObject: testingHeaders,
-      newDeclarationBodyString: DeclarationBody(
-              arrivalDate: DateTime.now(),
-              departureDate: DateTime.now().add(const Duration(days: 3)),
-              payout: 0.08,
-              platform: "Airbnb",
-              viewState: (() {
-                return null;
-              })())
-          .bodyString,
+    final newDeclarationPage = await http.get(
+      //?We get a new ViewState from here
+      Uri.parse(
+        "https://www1.aade.gr/taxisnet/short_term_letting/views/declaration.xhtml?propertyId=$samplePropertyId",
+      ),
+      headers: testingHeaders.getHeadersForGET(),
     );
 
-    print(res1.body); //!! returns a ViewState.
-    print(res1.statusCode);
+    final viewState =
+        getBetweenStrings(newDeclarationPage.body, 'ViewState" value="', '"');
 
-    expect(res1.statusCode, 200); //TODO make an actual test.
+    final testDeclarationBody = DeclarationBody(
+      arrivalDate: DateTime.now(),
+      departureDate: DateTime.now().add(const Duration(days: 3)),
+      payout: 0.01,
+      platform: "Airbnb",
+      viewState: viewState,
+    );
+
+    await setRenterName(testingHeaders,
+        testDeclarationBody); //? This renter name is set automatically when we 'change' the afm field. In most cases you'll set a 000 000 000 afm.
+
+    final res1 = await postNewDeclarationRequest(
+      headersObject: testingHeaders,
+      newDeclarationBodyString: testDeclarationBody.bodyString,
+    );
+
+    final errorMessage =
+        getBetweenStrings(res1.body, 'ui-messages-error-detail">', "</span>");
+    expect(errorMessage, "");
+    final infoMessage =
+        getBetweenStrings(res1.body, 'ui-messages-info-detail">', "</span>");
+    expect(infoMessage, "Επιτυχής Αποθήκευση");
   });
 }
 
-Future<Response> createNewDeclarationRequest({
+Future<void> setRenterName(DeclarationsPageHeaders testingHeaders,
+    DeclarationBody testDeclarationBody) async {
+  await http.post(
+    Uri.parse(
+      "https://www1.aade.gr/taxisnet/short_term_letting/views/declaration.xhtml",
+    ),
+    headers: testingHeaders.getHeadersForPOST(),
+    body:
+        "javax.faces.partial.ajax=true&javax.faces.source=appForm%3ArenterAfm&javax.faces.partial.execute=appForm%3ArenterAfm&javax.faces.partial.render=appForm%3Amessages+appForm%3ArenterAfm+appForm%3ArenterName&javax.faces.behavior.event=change&javax.faces.partial.event=change&appForm=appForm&appForm%3ArentalFrom_input=&appForm%3ArentalTo_input=&appForm%3AsumAmount_input=&appForm%3AsumAmount_hinput=&appForm%3ApaymentType_input=&appForm%3ApaymentType_focus=&appForm%3Aplatform_input=&appForm%3Aplatform_focus=&appForm%3ArenterAfm=000000000&appForm%3AcancelAmount_input=&appForm%3AcancelAmount_hinput=&appForm%3AcancelDate_input=&appForm%3Aj_idt93=&javax.faces.ViewState=${testDeclarationBody.viewStateParsed}",
+  );
+}
+
+Future<Response> postNewDeclarationRequest({
   required DeclarationsPageHeaders headersObject,
   required String newDeclarationBodyString,
 }) async {
   return http.post(
     Uri.https(
-        "www1.aade.gr", "taxisnet/short_term_letting/views/declaration.xhtml"),
+      "www1.aade.gr",
+      "taxisnet/short_term_letting/views/declaration.xhtml",
+    ),
     headers: headersObject.getHeadersForPOST(),
     body: newDeclarationBodyString,
   );
