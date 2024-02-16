103517990f2c69640a1ee6af60fbdc36400ed1af
deadlyunicorn <petrache.dev@gmail.com>
2024-02-16 21:25:56 +0200

    isLoggedIn now is calculated based on stored userCredentials( ChangeNotifier )

--- lib/declarations/declarations_page.dart
+++ lib/declarations/declarations_page.dart
@@ -1,74 +1,40 @@
 import 'package:decla_time/core/constants/constants.dart';
 import 'package:decla_time/declarations/login/login_form.dart';
+import 'package:decla_time/declarations/login/user_credentials_provider.dart';
+import 'package:decla_time/declarations/synced_declarations.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class DeclarationsPage extends StatefulWidget {
+class DeclarationsPage extends StatelessWidget {
   const DeclarationsPage({super.key, required this.localized});
 
   final AppLocalizations localized;
   @override
-  State<DeclarationsPage> createState() => _DeclarationsPageState();
-}
-
-class _DeclarationsPageState extends State<DeclarationsPage> {
-  bool isLogged = false;
-  bool isLoading = false;
-
-  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
-      child: Column(
-        children: [
-          const SizedBox.square(
-            dimension: 32,
-          ),
-          Expanded(
+    final isLoggedIn =
+        context.watch<DeclarationsAccountNotifier>().userCredentials != null;
+
+    return isLoggedIn
+        ? const SyncedDeclarations()
+        : Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: Center(
               child: FutureBuilder<String>(
-                future: getSavedUsernameFuture(),
-                builder: (context, snapshot) {
-                  final username = snapshot.data;
+                  future: getSavedUsernameFuture(),
+                  builder: (context, snapshot) {
+                    final username = snapshot.data;
 
-                  if (snapshot.connectionState == ConnectionState.done &&
-                      !isLoading) {
-                    return LoginForm(
-                      initialUsername: username ?? "",
-                      localized: widget.localized,
-                      setLoginStatus: setLoginStatus,
-                      setLoadingStatus: setLoadingStatus,
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                },
-              ),
+                    return snapshot.connectionState == ConnectionState.done
+                        ? LoginForm(
+                            initialUsername: username ?? "",
+                            localized: localized,
+                          )
+                        : const CircularProgressIndicator();
+                  }),
             ),
-          ),
-          const SizedBox.square(
-            dimension: 32,
-          ),
-        ],
-      ),
-    );
-  }
-
-  void setLoginStatus(bool newLoginStatus) {
-    if (isLogged != newLoginStatus) {
-      setState(() {
-        isLogged = newLoginStatus;
-      });
-    }
-  }
-
-  void setLoadingStatus(bool newLoadingStatus) {
-    if (isLoading != newLoadingStatus) {
-      setState(() {
-        isLoading = newLoadingStatus;
-      });
-    }
+          );
   }
 
   Future<String> getSavedUsernameFuture() async {
