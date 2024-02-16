bcc0b628f841fac2791673ae6be45a8d86f9c4e3
deadlyunicorn <petrache.dev@gmail.com>
2024-02-16 21:16:29 +0200

    added excepetions that may occur while logging in

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,2 +1,4 @@
 class ServerException implements Exception{}
 class UnsupportedFileException implements Exception{}
+class LoginFailedExcepetion implements Exception{}
+class NotLoggedInException implements Exception{}
