3d7c4319347f4fde5e4c00012234245c9b45bfd2
deadlyunicorn <petrache.dev@gmail.com>
2024-02-16 21:18:15 +0200

    checkIfLoggedIn now throws Exception

--- lib/declarations/functions/check_if_logged_in.dart
+++ lib/declarations/functions/check_if_logged_in.dart
@@ -1,7 +1,9 @@
+import 'package:decla_time/core/errors/exceptions.dart';
+
 void checkIfLoggedIn(String body) {
   if (body.contains(
     "<b>Απαγορεύεται η μη εξουσιοδοτημένη χρήση αυτής της τοποθεσίας,<br/>η οποία μπορεί να επιφέρει αστική και ποινική δίωξη.</b>",
   )) {
-    throw "Logged out.";
+    throw NotLoggedInException();
   }
 }
