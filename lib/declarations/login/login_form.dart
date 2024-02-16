bcfd0024d8cb402efa2223dcf85266c05386ed01
deadlyunicorn <petrache.dev@gmail.com>
2024-02-16 19:16:20 +0200

    replaced Wrap with ColumnWithSpacings. Added a setLoadingStatus function

--- lib/declarations/login/login_form.dart
+++ lib/declarations/login/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:decla_time/core/constants/constants.dart';
+import 'package:decla_time/core/widgets/column_with_spacings.dart';
 import 'package:decla_time/declarations/login/remember_username_checkbox.dart';
 import 'package:decla_time/reservations/presentation/widgets/reservation_form/form_fields/required_text_field.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -9,11 +10,13 @@ class LoginForm extends StatefulWidget {
   const LoginForm({
     super.key,
     required this.setLoginStatus,
+    required this.setLoadingStatus,
     required this.localized,
     required this.initialUsername,
   });
 
   final void Function(bool newStatus) setLoginStatus;
+  final void Function(bool newStatus) setLoadingStatus;
   final AppLocalizations localized;
   final String initialUsername;
 
@@ -31,70 +34,69 @@ class _LoginFormState extends State<LoginForm> {
   void initState() {
     super.initState();
     usernameController.text = widget.initialUsername;
-    if( widget.initialUsername.isNotEmpty ){
+    if (widget.initialUsername.isNotEmpty) {
       rememberUsername = true;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Wrap(
-        runSpacing: 16,
-        spacing: 16,
-        direction: Axis.vertical,
-        children: [
-          SizedBox(
-            width: kFormFieldWidth,
-            child: Text(
-              "Login Form",
-              style: Theme.of(context).textTheme.headlineMedium,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          RequiredTextField(
-            localized: widget.localized,
-            label: "Username",
-            controller: usernameController,
-          ),
-          RequiredTextField(
-            localized: widget.localized,
-            label: "password",
-            controller: passwordController,
-            obscureText: true,
-          ),
-          RememberUsernameCheckbox(
-            rememberUsername: rememberUsername,
-            setRememberUsername: setRememberUsername,
-          ),
-          SizedBox(
-            width: kFormFieldWidth,
-            child: Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    final username = usernameController.text;
-                    final password = passwordController.text;
-
-                    SharedPreferences.getInstance().then((prefs) {
-                      prefs.setString(
-                        kGovUsername,
-                        rememberUsername ? username : "",
-                      );
-                    });
-
-                    widget.setLoginStatus(true);
-                  }
-                },
-                child: Text(
-                  "Log in",
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
+    return SizedBox(
+      width: kFormFieldWidth,
+      child: Form(
+        key: _formKey,
+        child: ColumnWithSpacings(
+          spacing: 16,
+          children: [
+            SizedBox(
+              child: Text(
+                "Login Form",
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-        ],
+            RequiredTextField(
+              localized: widget.localized,
+              label: "Username",
+              controller: usernameController,
+            ),
+            RequiredTextField(
+              localized: widget.localized,
+              label: "password",
+              controller: passwordController,
+              obscureText: true,
+            ),
+            RememberUsernameCheckbox(
+              rememberUsername: rememberUsername,
+              setRememberUsername: setRememberUsername,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  widget.setLoadingStatus(true);
+            
+                  final username = usernameController.text;
+                  final password = passwordController.text;
+            
+                  SharedPreferences.getInstance().then((prefs) {
+                    prefs.setString(
+                      kGovUsername,
+                      rememberUsername ? username : "",
+                    );
+                  });
+            
+                  await Future.delayed(const Duration(seconds: 2));
+                  widget.setLoginStatus(true);
+                }
+                widget.setLoadingStatus(false);
+              },
+              child: Text(
+                "Log in",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
