f8e8eb6d80caddcb492c1f19c23cf1dfc5942bcc
deadlyunicorn <petrache.dev@gmail.com>
2024-02-17 18:44:34 +0200

    UsersDrawer now is a column with spacings. Also added related Widgets.

--- lib/users/drawer/users_drawer.dart
+++ lib/users/drawer/users_drawer.dart
@@ -1,22 +1,43 @@
+import 'package:decla_time/core/widgets/column_with_spacings.dart';
+import 'package:decla_time/declarations/database/user/user.dart';
+import 'package:decla_time/users/drawer/add_user_button.dart';
 import 'package:decla_time/users/drawer/drawer_outline.dart';
+import 'package:decla_time/users/drawer/users_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class UsersDrawer extends StatelessWidget {
   const UsersDrawer({
     super.key,
+    required this.localized,
   });
+
+  final AppLocalizations localized;
 
   @override
   Widget build(BuildContext context) {
+    final List<User> users = []; // context.watch<UsersController>().users;
+
     return DrawerOutline(
-      child: Column(
-                children: [
-                  Text("Users",style: Theme.of(context).textTheme.headlineMedium,),
-                  Text("Hello world"),
-                  Text("Hello world"),
-                  Text("Hello world"),
-                ],
-              ),
+      child: ColumnWithSpacings(
+        spacing: 16,
+        children: [
+          Text(
+            "Users",
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox.square(
+            dimension: 16,
+          ),
+          UsersList(
+            users: users,
+            localized: localized,
+          ),
+          AddUserButton(
+            localized: localized,
+          ),
+        ],
+      ),
     );
   }
 }
