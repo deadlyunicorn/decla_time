e41b2e46e9041d5fc687e53c56426709f4239ebc
deadlyunicorn <petrache.dev@gmail.com>
2024-02-17 12:52:36 +0200

    added the schemas inside Isar.open(). Added declarationActions getter

--- lib/core/connection/isar_helper.dart
+++ lib/core/connection/isar_helper.dart
@@ -1,13 +1,23 @@
+import 'package:decla_time/core/connection/declaration_actions.dart';
 import 'package:decla_time/core/connection/reservation_actions.dart';
 import 'package:decla_time/core/documents_io/documents_io.dart';
+import 'package:decla_time/declarations/database/declaration.dart';
+import 'package:decla_time/declarations/database/finalized_declaration_details.dart';
+import 'package:decla_time/declarations/database/user/user.dart';
+import 'package:decla_time/declarations/database/user/user_property.dart';
 import 'package:decla_time/reservations/reservation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:isar/isar.dart';
 
 class IsarHelper extends ChangeNotifier {
   Future<Isar> get isarFuture async => Isar.instanceNames.isEmpty
-      ? await Isar.open([ReservationSchema],
-          directory: (await DocumentsIO.appDirFuture).path)
+      ? await Isar.open([
+          ReservationSchema,
+          DeclarationSchema,
+          FinalizedDeclarationDetailsSchema,
+          UserPropertySchema,
+          UserSchema,
+        ], directory: (await DocumentsIO.appDirFuture).path)
       : await Future.value(Isar.getInstance()!);
 
   //? The only bad thing about this design is that in the future
@@ -18,6 +28,10 @@ class IsarHelper extends ChangeNotifier {
 
   //? This is basically a notifier that listens for all database changes..
   ReservationActions get reservationActions => ReservationActions(
+        isarFuture: isarFuture,
+        notifyListeners: notifyListeners,
+      );
+  DeclarationActions get declarationActions => DeclarationActions(
         isarFuture: isarFuture,
         notifyListeners: notifyListeners,
       );
