66b30a582e4cf5bed1b20be8e638e643ed71cd72
deadlyunicorn <petrache.dev@gmail.com>
2024-02-17 12:52:52 +0200

    added an errorSnackBar

---
+++ lib/core/functions/show_error_snackbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void showErrorSnackbar({
+  required BuildContext context,
+  required String message,
+}) {
+  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Center(
+        child: Text(message),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.error,
+
+    ),
+  );
+}
