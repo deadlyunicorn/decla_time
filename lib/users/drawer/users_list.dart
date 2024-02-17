0d3f5797271b02cc2f7ee4e932387ce7368092d0
deadlyunicorn <petrache.dev@gmail.com>
2024-02-17 19:37:49 +0200

    added more things to the users list

--- lib/users/drawer/users_list.dart
+++ lib/users/drawer/users_list.dart
@@ -1,7 +1,9 @@
 import 'package:decla_time/core/extensions/capitalize.dart';
 import 'package:decla_time/declarations/database/user/user.dart';
+import 'package:decla_time/users/users_controller.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UsersList extends StatelessWidget {
   const UsersList({
@@ -15,6 +17,9 @@ class UsersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //TODO IN PROGRESS
+    final selectedUser = context.watch<UsersController>().selectedUser;
     if (users.isEmpty) {
       return Column(
         children: [
@@ -26,9 +31,56 @@ class UsersList extends StatelessWidget {
       );
     } else {
       return ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (context, index) => Text(users[index].username),
-      );
+          shrinkWrap: true,
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final bool isCurrentlySelectedUser =
+                selectedUser == users[index].username;
+
+            return Column(
+              children: [
+                ListTile(
+                  title: RichText(
+                    text: TextSpan(
+                        text: users[index].username,
+                        style:
+                            Theme.of(context).textTheme.bodyLarge?.copyWith(),
+                        children: isCurrentlySelectedUser
+                            ? [
+                                TextSpan(
+                                    text: "logged in", //TODO change this.
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodySmall
+                                        ?.copyWith(
+                                          fontSize: 12,
+                                          color: isCurrentlySelectedUser
+                                              ? Theme.of(context)
+                                                  .colorScheme
+                                                  .surface
+                                              : Theme.of(context)
+                                                  .colorScheme
+                                                  .onBackground,
+                                        ))
+                              ]
+                            : []),
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: SizedBox.square( dimension: 4,),
+                        title: Text(
+                      "selecteed property",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ));
+                  },
+                  itemCount: 2,
+                )
+              ],
+            );
+          });
     }
   }
 }
