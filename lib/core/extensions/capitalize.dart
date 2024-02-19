31c45f0170c3125ae7d44229f7612fee54963982
deadlyunicorn <petrache.dev@gmail.com>
2024-02-19 16:17:03 +0200

    added an extension that capitalizes all of the words of a phrase

--- lib/core/extensions/capitalize.dart
+++ lib/core/extensions/capitalize.dart
@@ -1,8 +1,24 @@
-extension CapitalizeString on String{
+extension CapitalizeString on String {
+  String get capitalized => length > 1
+      ? "${this[0].toUpperCase()}${substring(1).toLowerCase()}"
+      : length == 1
+          ? this[0].capitalized
+          : "";
 
-  String get capitalized  => length > 1 
-    ? "${this[0].toUpperCase()}${substring(1).toLowerCase()}"
-    : length == 1 
-      ? this[0].capitalized
-      : "";
+  String get capitalizedAll => length > 1
+      ? (() {
+          final tempString = [];
+          for (int i = 0; i < runes.length; i++) {
+            if (i == 0) {
+              tempString.add(this[i].toUpperCase());
+              continue;
+            }
+            tempString
+                .add((this[i - 1] == " ") ? this[i].toUpperCase() : this[i]);
+          }
+          return tempString.join("");
+        })()
+      : length == 1
+          ? this[0].capitalized
+          : "";
 }
