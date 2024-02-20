010276c226b70a84fda846ca9cc6b919c2dca01c
deadlyunicorn <petrache.dev@gmail.com>
2024-02-20 19:12:39 +0200

    Created a snackbars.dart file containing showErrorSnackBar and showNormalSnackbar.

--- lib/core/functions/snackbars.dart
+++ lib/core/functions/snackbars.dart
@@ -11,7 +11,20 @@ void showErrorSnackbar({
         child: Text(message),
       ),
       backgroundColor: Theme.of(context).colorScheme.error,
+    ),
+  );
+}
 
+void showNormalSnackbar({
+  required BuildContext context,
+  required String message,
+}) {
+  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Center(
+        child: Text(message),
+      ),
     ),
   );
 }
