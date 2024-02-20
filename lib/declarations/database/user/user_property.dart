ae088219520f804b40696a397b2747b0db43afd8
deadlyunicorn <petrache.dev@gmail.com>
2024-02-20 19:17:05 +0200

    moved the generateFromHtml inside the userProperty class

--- lib/declarations/database/user/user_property.dart
+++ lib/declarations/database/user/user_property.dart
@@ -1,3 +1,4 @@
+import 'package:decla_time/core/extensions/get_values_between_strings.dart';
 import 'package:decla_time/core/functions/fasthash.dart';
 import 'package:isar/isar.dart';
 
@@ -25,4 +26,35 @@ class UserProperty {
     required this.serialNumber,
     this.friendlyName,
   });
+
+  static List<UserProperty> generateFromHtml(String body) {
+    final propertyIds = getAllBetweenStrings(body, "propertyId',value:'", "'}");
+    if (propertyIds.isEmpty) return [];
+    final atakNumbers = getAllBetweenStrings(
+        body, 'atakOutput" style="width:90px;">', "</span>");
+    final addressOfProperties =
+        getAllBetweenStrings(body, 'addressOutput">', "<")
+            .map((addressEntry) => addressEntry.trim())
+            .toList();
+    final registryNumbers =
+        getAllBetweenStrings(body, 'amaOutput" style="width:90px;">', '<');
+
+    if (propertyIds.length != atakNumbers.length &&
+        atakNumbers.length != addressOfProperties.length &&
+        addressOfProperties.length != registryNumbers.length) {
+      return [];
+    } else {
+      final List<UserProperty> properties = [];
+      for (int i = 0; i < propertyIds.length; i++) {
+        properties.add(
+          UserProperty(
+              propertyId: propertyIds[i],
+              address: addressOfProperties[i],
+              atak: atakNumbers[i],
+              serialNumber: registryNumbers[i]),
+        );
+      }
+      return properties;
+    }
+  }
 }
