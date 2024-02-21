3c82317f818e92d2f3c0a354dff7336d09e49277
deadlyunicorn <petrache.dev@gmail.com>
2024-02-21 19:11:02 +0200

    added some initial declarationDbActions

--- lib/core/connection/declaration_actions.dart
+++ lib/core/connection/declaration_actions.dart
@@ -1,3 +1,4 @@
+import 'package:decla_time/declarations/database/declaration.dart';
 import 'package:isar/isar.dart';
 
 class DeclarationActions {
@@ -7,5 +8,69 @@ class DeclarationActions {
   DeclarationActions({required isarFuture, required notifyListeners})
       : _isarFuture = isarFuture,
         _notifyListeners = notifyListeners;
-  
+
+  Future<List<Declaration>> getAllEntriesFromDeclarations() async {
+    return (await _isarFuture)
+        .declarations
+        .where()
+        .sortByDepartureDateDesc()
+        .findAll();
+  }
+
+  Future<void> removeFromDatabase(int declarationDbId) async {
+    final isar = await _isarFuture;
+    await isar.writeTxn(() async {
+      await isar.declarations.deleteByDeclarationDbId(declarationDbId);
+    });
+    _notifyListeners();
+  }
+
+  // Future<Iterable<Declaration>> filterRegistered(
+  //     Iterable<Declaration> declarations) async {
+  //   final Iterable<Declaration> databaseResponse = await (await _isarFuture)
+  //       .declarations
+  //       .getAllById(
+  //         declarations.map((reservation) => reservation.id).toList(),
+  //       )
+  //       .then(
+  //         (databaseDeclarations) => databaseDeclarations.nonNulls,
+  //       );
+  //   return databaseResponse;
+  // }
+
+  Future<bool> declarationAlreadyExists(int declarationDbId) async {
+    final Declaration? databaseResponse =
+        await (await _isarFuture).declarations.getByDeclarationDbId(
+              declarationDbId,
+            );
+    return databaseResponse != null;
+  }
+
+  Future<void> insertOrUpdateDeclarationEntry(Declaration declaration) async {
+    final isar = await _isarFuture;
+
+    await isar.writeTxn(() async {
+      await isar.declarations.put(declaration);
+    });
+    _notifyListeners();
+  }
+
+  Future<Declaration?> getDeclarationEntry(int declarationDbId) async {
+    final isar = await _isarFuture;
+    return await isar.declarations
+        .filter()
+        .declarationDbIdEqualTo(declarationDbId)
+        .findFirst();
+  }
+
+  Future<void> insertMultipleEntriesToDb(List<Declaration> declarations) async {
+    final isar = await _isarFuture;
+
+    await isar.writeTxn(() async {
+      for (final declaration in declarations) {
+        await isar.declarations.put(declaration);
+      }
+    });
+    _notifyListeners();
+  }
 }
