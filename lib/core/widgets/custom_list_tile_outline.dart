de23e88707e19653a3fe4bd7f125b3bd5fb97b82
deadlyunicorn <petrache.dev@gmail.com>
2024-02-21 17:16:47 +0200

    added an isSelected property for the CustomListOutline.

--- lib/core/widgets/custom_list_tile_outline.dart
+++ lib/core/widgets/custom_list_tile_outline.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CustomListTileOutline extends StatelessWidget {
-  const CustomListTileOutline({super.key, required this.child});
+  const CustomListTileOutline({
+    super.key,
+    required this.child,
+    this.isSelected = false,
+  });
 
   final ListTile child;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: isSelected ? Theme.of(context).colorScheme.primary : null,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       clipBehavior: Clip.antiAlias,
       child: InkWell(child: child),
