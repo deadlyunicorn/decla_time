30400b6ad02f4c395882185ce2b5db6d463faf0a
deadlyunicorn <petrache.dev@gmail.com>
2024-02-22 17:45:37 +0200

    added mainAxis properties for the column with spacings.

--- lib/core/widgets/column_with_spacings.dart
+++ lib/core/widgets/column_with_spacings.dart
@@ -5,18 +5,23 @@ class ColumnWithSpacings extends StatelessWidget {
     super.key,
     required this.children,
     required this.spacing,
+    this.mainAxisSize = MainAxisSize.max,
+    this.mainAxisAlignment = MainAxisAlignment.start,
   });
 
   final List<Widget> children;
   final int spacing;
-
+  final MainAxisSize mainAxisSize;
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: mainAxisSize,
+      mainAxisAlignment: mainAxisAlignment,
       children: children
           .map(
             (child) => Padding(
-              padding: EdgeInsets.only(bottom: spacing.toDouble() ),
+              padding: EdgeInsets.only(bottom: spacing.toDouble()),
               child: child,
             ),
           )
