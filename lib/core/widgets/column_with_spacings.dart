4e84154391f83f69e1ef86e152ef3dbd09ebc13b
deadlyunicorn <petrache.dev@gmail.com>
2024-02-23 23:28:47 +0200

    added crossAxis to ColumnWithSpacings

--- lib/core/widgets/column_with_spacings.dart
+++ lib/core/widgets/column_with_spacings.dart
@@ -6,18 +6,20 @@ class ColumnWithSpacings extends StatelessWidget {
     required this.children,
     required this.spacing,
     this.mainAxisSize = MainAxisSize.max,
-    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisAlignment = MainAxisAlignment.start, this.crossAxisAlignment = CrossAxisAlignment.center,
   });
 
   final List<Widget> children;
   final int spacing;
   final MainAxisSize mainAxisSize;
   final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: mainAxisSize,
       mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
       children: children
           .map(
             (child) => Padding(
