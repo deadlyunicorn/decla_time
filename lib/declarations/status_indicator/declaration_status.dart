e68aa896dc6ef28a5523d0e3a5a89e6fc9aa3c4e
deadlyunicorn <petrache.dev@gmail.com>
2024-02-26 19:03:03 +0200

    added ( but not 100% sure if will use ) a declaration status class

---
+++ lib/declarations/status_indicator/declaration_status.dart
@@ -0,0 +1,13 @@
+class DeclarationImportStatus {
+  final bool imported;
+  final double payout;
+  final DateTime arrivalDate;
+  final DateTime departureDate;
+
+  DeclarationImportStatus({
+    required this.imported,
+    required this.payout,
+    required this.arrivalDate,
+    required this.departureDate,
+  });
+}
