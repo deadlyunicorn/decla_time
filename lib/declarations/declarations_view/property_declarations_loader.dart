d9b9ee805539c793b80d3e36cfdd97a4facdd88a
deadlyunicorn <petrache.dev@gmail.com>
2024-02-27 19:10:17 +0200

    unfinished declaration display. ( will use gridview similar to reservations.. )

--- lib/declarations/declarations_view/property_declarations_loader.dart
+++ lib/declarations/declarations_view/property_declarations_loader.dart
@@ -1,8 +1,13 @@
 import "package:decla_time/core/connection/isar_helper.dart";
+import "package:decla_time/core/extensions/capitalize.dart";
 import "package:decla_time/declarations/database/declaration.dart";
 import "package:decla_time/declarations/declarations_view/declaration_actions.dart";
+import "package:decla_time/declarations/status_indicator/declaration_sync_controller.dart";
+import "package:decla_time/reservations/presentation/reservations_list/reservation_of_month_grid_view.dart";
+import "package:decla_time/reservations/presentation/reservations_list/reservations_of_year_list.dart";
 import "package:decla_time/users/users_controller.dart";
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
 import "package:provider/provider.dart";
 
@@ -27,15 +32,22 @@ class PropertyDeclarationsLoader extends StatelessWidget {
           .getAllDeclarationsFrom(propertyId: selectedPropertyId),
       builder: (_, AsyncSnapshot<List<Declaration>> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
+          final List<Declaration> declarations =
+              snapshot.data ?? <Declaration>[];
+
           return Column(
             children: <Widget>[
               DeclarationActions(
                 localized: localized,
                 selectedPropertyId: selectedPropertyId,
+                totalDeclarations: declarations.length,
               ),
-              const SizedBox(
-                height: 450,
-                child: Center(child: Text("Declarations Below")),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: declarations.length,
+                itemBuilder: (BuildContext context, int index) => Text(
+                  declarations[0].isarId.toString(),
+                ),
               ),
             ],
           );
