81f6d7ba73d88fe9acc437739a4dae92e2b21032
deadlyunicorn <petrache.dev@gmail.com>
2024-03-01 18:34:24 +0200

    throw TryAgainLater before NotLoggedIn

--- lib/declarations/functions/check_if_logged_in.dart
+++ lib/declarations/functions/check_if_logged_in.dart
@@ -2,12 +2,12 @@ import "package:decla_time/core/errors/exceptions.dart";
 
 void checkIfLoggedIn(String body) {
   if (body.contains(
-    "<b>Απαγορεύεται η μη εξουσιοδοτημένη χρήση αυτής της τοποθεσίας,<br/>η οποία μπορεί να επιφέρει αστική και ποινική δίωξη.</b>",
-  )) {
-    throw NotLoggedInException();
-  } else if (body.contains(
     "Ο χρήστης χρησιμοποιεί ήδη το μέγιστο αριθμό περιόδων λειτουργίας.",
   )) {
     throw TryAgainLaterException();
+  } else if (body.contains(
+    "<b>Απαγορεύεται η μη εξουσιοδοτημένη χρήση αυτής της τοποθεσίας,<br/>η οποία μπορεί να επιφέρει αστική και ποινική δίωξη.</b>",
+  )) {
+    throw NotLoggedInException();
   }
 }
