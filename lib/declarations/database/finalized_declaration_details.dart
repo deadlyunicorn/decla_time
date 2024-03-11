3646da242ac32e41ccfaf795701a3c67d09e22a3
deadlyunicorn <petrache.dev@gmail.com>
2024-03-11 13:32:54 +0200

    added a "getDeclarationTypeLocalized" static function

--- lib/declarations/database/finalized_declaration_details.dart
+++ lib/declarations/database/finalized_declaration_details.dart
@@ -1,5 +1,6 @@
 import "package:decla_time/core/enums/declaration_type.dart";
 import "package:decla_time/core/functions/fasthash.dart";
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
 import "package:isar/isar.dart";
 
 part "finalized_declaration_details.g.dart";
@@ -42,5 +43,17 @@ class FinalizedDeclarationDetails {
         finalizedDeclarationDetails.declarationDbId == declarationDbId &&
         finalizedDeclarationDetails.serialNumberOfAmendingDeclaration ==
             serialNumberOfAmendingDeclaration;
+  }
+
+  static String getDeclarationTypeLocalized({
+    required AppLocalizations localized,
+    required DeclarationType type,
+  }) {
+    switch (type) {
+      case DeclarationType.amending:
+        return localized.declarationType_amending;
+      case DeclarationType.initial:
+        return localized.declarationType_initial;
+    }
   }
 }
