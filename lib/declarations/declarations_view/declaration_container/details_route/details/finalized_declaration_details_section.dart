6706ac831a61d88bbc4d40d56cbc4e6ef9950757
deadlyunicorn <petrache.dev@gmail.com>
2024-03-13 13:10:22 +0200

    added link to the amended declaration

--- lib/declarations/declarations_view/declaration_container/details_route/details/finalized_declaration_details_section.dart
+++ lib/declarations/declarations_view/declaration_container/details_route/details/finalized_declaration_details_section.dart
@@ -1,10 +1,17 @@
+import "dart:async";
+
+import "package:decla_time/core/connection/isar_helper.dart";
 import "package:decla_time/core/enums/declaration_type.dart";
 import "package:decla_time/core/extensions/capitalize.dart";
+import "package:decla_time/core/functions/snackbars.dart";
 import "package:decla_time/core/widgets/column_with_spacings.dart";
+import "package:decla_time/declarations/database/declaration.dart";
 import "package:decla_time/declarations/database/finalized_declaration_details.dart";
+import "package:decla_time/declarations/declarations_view/declaration_container/declaration_details_route.dart";
 import "package:decla_time/reservations/presentation/reservations_list/reservation_details_route/date_information_widget.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:provider/provider.dart";
 
 class FinalizedDeclarationDetailsSection extends StatelessWidget {
   const FinalizedDeclarationDetailsSection({
@@ -30,21 +37,52 @@ class FinalizedDeclarationDetailsSection extends StatelessWidget {
               "${localized.declarationDate.capitalized}: ${DateInformationWidget.dateFormat(declarationDetails.declarationDate)}",
             ),
             Text(
+              // ignore: lines_longer_than_80_chars
               "${localized.declarationType.capitalized}: ${FinalizedDeclarationDetails.getDeclarationTypeLocalized(
                 localized: localized,
                 type: declarationDetails.declarationType,
               ).capitalized}",
             ),
-            if (declarationDetails.declarationType == DeclarationType.amending)
+            if (declarationDetails.declarationType ==
+                    DeclarationType.amending &&
+                declarationDetails.serialNumberOfAmendingDeclaration != null)
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   Text("${localized.amendingFor.capitalized}:"),
                   TextButton(
-                    onPressed: () {
-                      print("take us to a route with the amending declaration");
+                    onPressed: () async {
+                      final Declaration? amendedDeclaration = await context
+                          .read<IsarHelper>()
+                          .declarationActions
+                          .getDeclarationBySerialNumber(
+                            declarationDetails
+                                .serialNumberOfAmendingDeclaration!,
+                          );
+                      if (context.mounted) {
+                        if (amendedDeclaration != null) {
+                          unawaited(
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute<void>(
+                                builder: (BuildContext context) =>
+                                    DeclarationDetailsRoute(
+                                  localized: localized,
+                                  declaration: amendedDeclaration,
+                                ),
+                              ),
+                            ),
+                          );
+                        } else {
+                          showErrorSnackbar(
+                            context: context,
+                            message: localized.itemNotFoundLocally.capitalized,
+                          );
+                        }
+                      }
                     },
                     child: Text(
+                      // ignore: lines_longer_than_80_chars
                       "${localized.serialNumberShort}: ${declarationDetails.serialNumberOfAmendingDeclaration!}",
                     ),
                   ),
