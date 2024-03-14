7d9fa55a8c409d68e02800368fa9b6a8942de9ad
deadlyunicorn <petrache.dev@gmail.com>
2024-03-15 01:41:13 +0200

    Moved the futureBuilder that checks if a reservation to be imported, already exists in the db.

--- lib/reservations_action_button_menu/entries_found/will_overwrite_tooltip.dart
+++ lib/reservations_action_button_menu/entries_found/will_overwrite_tooltip.dart
@@ -1,31 +1,50 @@
+import "package:decla_time/core/connection/isar_helper.dart";
 import "package:decla_time/core/extensions/capitalize.dart";
+import "package:decla_time/reservations/reservation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:provider/provider.dart";
 
 class WillOverwriteTooltip extends StatelessWidget {
   const WillOverwriteTooltip({
-    required this.reservationAlreadyInDatabase,
+    required this.reservation,
     required this.localized,
     super.key,
   });
 
-  final bool reservationAlreadyInDatabase;
+  final Reservation reservation;
   final AppLocalizations localized;
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: 0,
-      bottom: 0,
-      child: reservationAlreadyInDatabase
-          ? Tooltip(
-              message: localized.willOverwriteExistingReservation.capitalized,
-              child: Icon(
-                Icons.edit_document,
-                color: Theme.of(context).colorScheme.error,
-              ),
-            )
-          : const SizedBox.shrink(),
+    return FutureBuilder<Iterable<Reservation>>(
+      future: context
+          .watch<IsarHelper>()
+          .reservationActions
+          .filterRegistered(<Reservation>[reservation]), 
+          //?This was filtering all with one query, but the future builder 
+          //?was preventing generalization.
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<Iterable<Reservation>> snapshot,
+      ) {
+        final bool alreadyExistsInDatabase =
+            (snapshot.data ?? <Reservation>[]).isNotEmpty;
+        return Positioned(
+          left: 0,
+          bottom: 0,
+          child: alreadyExistsInDatabase
+              ? Tooltip(
+                  message:
+                      localized.willOverwriteExistingReservation.capitalized,
+                  child: Icon(
+                    Icons.edit_document,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+                )
+              : const SizedBox.shrink(),
+        );
+      },
     );
   }
 }
