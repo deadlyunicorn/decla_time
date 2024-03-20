4d5a91084f89c5f0bef5218ee6f7a1a1fa3e383a
deadlyunicorn <petrache.dev@gmail.com>
2024-03-20 15:14:12 +0200

    added an uknownErrorException

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -20,3 +20,6 @@ class UserPropertyNotFoundException implements Exception {}
 
 class AlreadySubmittingDeclarations implements Exception {}
 
+class UnknownErrorException implements Exception {}
+
+
