28a51be0153b6a516e540cac606cfb6e34827d2c
deadlyunicorn <petrache.dev@gmail.com>
2024-04-05 00:51:35 +0300

    on the declarationUploadingRoute:  added a `ClearSubmitted` button and an `ImportSubmittedDeclarations` buttons. Those appear when there are no pending declarations.

--- lib/declarations/status_indicator_declare/submitting_route/declaration_uploading_route.dart
+++ lib/declarations/status_indicator_declare/submitting_route/declaration_uploading_route.dart
@@ -1,5 +1,7 @@
 import "package:decla_time/core/extensions/capitalize.dart";
+import "package:decla_time/core/widgets/column_with_spacings.dart";
 import "package:decla_time/core/widgets/route_outline.dart";
+import "package:decla_time/declarations/declarations_view/synchronize_declarations/declaration_sync_range_picker_dialog.dart";
 import "package:decla_time/declarations/status_indicator_declare/declaration_submit_controller.dart";
 import "package:decla_time/declarations/status_indicator_declare/submitting_route/lists/pending_reservations.dart";
 import "package:decla_time/declarations/status_indicator_declare/submitting_route/lists/submitted_reservations_list.dart";
@@ -16,7 +18,6 @@ class DeclarationUploadingRoute extends StatelessWidget {
 
   final AppLocalizations localized;
 
-
   @override
   Widget build(BuildContext context) {
     final DeclarationSubmitController declarationsSubmitController =
@@ -28,14 +29,19 @@ class DeclarationUploadingRoute extends StatelessWidget {
         declarationsSubmitController.reservationsSubmitted;
 
     return RouteOutline(
-      title:
-          "${localized.declarationSubmit.capitalized}: ${submitted.length}/${submitted.length + pendingSubmissions.length}",
+      onExit: () {
+        if (pendingSubmissions.isEmpty) {
+          declarationsSubmitController.clearSubmitted();
+        }
+      },
+      title: pendingSubmissions.isEmpty && submitted.isEmpty
+          ? localized.noPendingOperations.capitalized
+          : "${localized.declarationSubmit.capitalized}: ${submitted.length}/${submitted.length + pendingSubmissions.length}",
       child: Column(
         children: <Widget>[
           Expanded(
             child: SingleChildScrollView(
               child: Column(
-                //TODO SinglePageScrollView
                 children: <Widget>[
                   SubmittedReservationsList(
                     localized: localized,
@@ -50,7 +56,7 @@ class DeclarationUploadingRoute extends StatelessWidget {
             ),
           ),
           if (pendingSubmissions.isEmpty &&
-              !submitted
+              submitted
                   .where(
                     (ReservationSubmitResult submitResult) =>
                         submitResult.wasSuccessful,
@@ -58,16 +64,89 @@ class DeclarationUploadingRoute extends StatelessWidget {
                   .isNotEmpty)
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: TextButton(
-                onPressed: () {
-                  //TODO Start importing
-                },
-                child: Text(
-                  localized.importSubmissions.capitalized,
-                ),
+              child: ColumnWithSpacings(
+                spacing: 4,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      declarationsSubmitController.clearSubmitted();
+                    },
+                    child: Text(
+                      localized.clear.capitalized,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  ),
+                  ImportSubmittedDeclarations(
+                    localized: localized,
+                  ),
+                ],
               ),
             ), //Find earliest and latest departure date.
         ],
+      ),
+    );
+  }
+}
+
+class ImportSubmittedDeclarations extends StatelessWidget {
+  const ImportSubmittedDeclarations({
+    required this.localized,
+    super.key,
+  });
+
+  final AppLocalizations localized;
+
+  @override
+  Widget build(BuildContext context) {
+    final DeclarationSubmitController declarationsSubmitController =
+        context.watch<DeclarationSubmitController>();
+    final List<ReservationSubmitResult> submitted =
+        declarationsSubmitController.reservationsSubmitted;
+
+    return TextButton(
+      onPressed: () {
+        final String? lastSubmittingPropertyId =
+            declarationsSubmitController.lastSubmittingPropertyId;
+        if (lastSubmittingPropertyId != null &&
+            lastSubmittingPropertyId.isNotEmpty) {
+          final List<ReservationSubmitResult>
+              declarationsToImportOrderedByDate = submitted
+                  .where(
+                    (ReservationSubmitResult declaration) =>
+                        declaration.wasSuccessful,
+                  )
+                  .toList()
+                ..sort(
+                  (
+                    ReservationSubmitResult a,
+                    ReservationSubmitResult b,
+                  ) =>
+                      a.reservation.departureDate.compareTo(
+                    b.reservation.departureDate,
+                  ),
+                );
+
+          final DateTime earliest = declarationsToImportOrderedByDate
+              .first.reservation.departureDate
+              .subtract(const Duration(days: 1));
+          final DateTime latest = declarationsToImportOrderedByDate
+              .last.reservation.departureDate
+              .add(const Duration(days: 1));
+
+          startImportingDeclarations(
+            context: context,
+            arrivalDate: earliest,
+            departureDateDate: latest,
+            propertyId: lastSubmittingPropertyId,
+          );
+
+          declarationsSubmitController.clearSubmitted();
+        }
+      },
+      child: Text(
+        localized.importSubmissions.capitalized,
       ),
     );
   }
