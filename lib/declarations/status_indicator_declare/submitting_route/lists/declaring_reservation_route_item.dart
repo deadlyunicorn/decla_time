37bd1defada82f9db16a323941943bb019bd08e1
deadlyunicorn <petrache.dev@gmail.com>
2024-04-05 01:56:47 +0300

    Added a unified Widget for pending and submitted Reservations in the ReservationSubmittingRoute

---
+++ lib/declarations/status_indicator_declare/submitting_route/lists/declaring_reservation_route_item.dart
@@ -0,0 +1,40 @@
+import "package:decla_time/core/functions/night_or_nights.dart";
+import "package:decla_time/core/widgets/column_with_spacings.dart";
+import "package:decla_time/reservations/reservation.dart";
+import "package:flutter/material.dart";
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
+
+class DeclaringReservationRouteItem extends StatelessWidget {
+  const DeclaringReservationRouteItem({
+    required this.reservation,
+    required this.localized,
+    super.key,
+  });
+
+  final Reservation reservation;
+  final AppLocalizations localized;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(reservation.guestName),
+        FittedBox(
+          child: Text(
+            // ignore: lines_longer_than_80_chars
+            "${reservation.arrivalDateString} - ${reservation.departureDateString}",
+          ),
+        ),
+        RowWithSpacings(
+          spacing: 16,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(nightOrNights(localized, reservation.nights)),
+            const Text("-"),
+            Text("${reservation.payout} EUR"),
+          ],
+        ),
+      ],
+    );
+  }
+}
