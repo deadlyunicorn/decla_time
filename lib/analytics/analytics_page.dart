b9839246b2771d5e56efb5918fe633dfaedc5213
deadlyunicorn <petrache.dev@gmail.com>
2024-04-05 16:54:23 +0300

    Wrapped the AnalyticsPage items inside a SingleChildScrollView and applied 4.51.1 4.51.2

--- lib/analytics/analytics_page.dart
+++ lib/analytics/analytics_page.dart
@@ -1,12 +1,9 @@
-import "package:decla_time/core/connection/isar_helper.dart";
-import "package:decla_time/core/constants/constants.dart";
-import "package:decla_time/core/extensions/text_button_error.dart";
-import "package:decla_time/settings.dart";
+import "package:decla_time/analytics/app_settings.dart";
+import "package:decla_time/analytics/graphs/analytics_graphs.dart";
+import "package:decla_time/core/extensions/capitalize.dart";
+import "package:decla_time/core/widgets/column_with_spacings.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
-import "package:isar/isar.dart";
-import "package:provider/provider.dart";
-import "package:shared_preferences/shared_preferences.dart";
 
 class AnalyticsPage extends StatelessWidget {
   const AnalyticsPage({
@@ -18,55 +15,21 @@ class AnalyticsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        const Expanded(child: Text("Analytics here")),
-        Center(
-          child: TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor: Theme.of(context).colorScheme.surface,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
-              ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32),
+        child: ColumnWithSpacings(  
+          spacing: 16,
+          children: <Widget>[
+            Text(
+              localized.analytics.capitalized,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-            onPressed: () {
-              context.read<SettingsController>().toggleLocale();
-            },
-            child: Text(
-              localized.localeFlag,
-            ),
-          ),
+            AnalyticsGraphs(localized: localized),
+            AppSettings(localized: localized),
+          ],
         ),
-        Center(
-          child: TextButton(
-            style: Theme.of(context).textButtonErrorTheme.style,
-            onPressed: () async {
-              final Future<Isar> isarFuture =
-                  context.read<IsarHelper>().isarFuture;
-              final Future<SharedPreferences> prefsFuture =
-                  SharedPreferences.getInstance();
-
-              final [
-                Isar isar as Isar,
-                SharedPreferences prefs as SharedPreferences
-              ] = await Future.wait(<Future<Object?>>[isarFuture, prefsFuture]);
-
-              await isar.writeTxn(() async {
-                await isar.clear();
-              });
-
-              await prefs.setStringList(kListings, <String>[]);
-              await prefs.setStringList(kBookingPlatforms, <String>[]);
-              await prefs.setStringList(kReservationListing, <String>[]);
-              await prefs.setStringList(kReservationStatus, <String>[]);
-            },
-            child: Text(
-              "Clear everything",
-              style: TextStyle(color: Theme.of(context).colorScheme.error),
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
