d4fdb4c8525b3d8c4f8c05d78fa9968caa052d6c
deadlyunicorn <petrache.dev@gmail.com>
2024-04-11 23:52:05 +0300

    renamed reservationsByMonth getter to an actual function and moved it inside its own file

---
+++ lib/analytics/graphs/revenue_per_month/business/get_reservations_by_month.dart
@@ -0,0 +1,36 @@
+import "package:decla_time/analytics/graphs/revenue_per_month/yearly_monthly_revenue_breakdown_chart.dart";
+import "package:decla_time/analytics/graphs/taxes_per_year/business/get_reservations_by_year.dart";
+import "package:decla_time/reservations/reservation.dart";
+
+///Probably computationally expensive. Don't call it too often.
+///Reservations are considered sorted beforehands.
+///it returns a list where each element is `{year,month,List<Reservation>}`
+List<ReservationsOfMonthOfYear> getReservationsByMonth(
+  List<ReservationsOfYear> reservationsGroupedByYear,
+) {
+  final List<ReservationsOfMonthOfYear> reservationsGroupedByMonth =
+      <ReservationsOfMonthOfYear>[];
+
+  for (int i = 0; i < reservationsGroupedByYear.length; i++) {
+    //reservations are considered already sorted
+    final ReservationsOfYear reservationsOfYear = reservationsGroupedByYear[i];
+    int currentMonth = -1;
+    // int indexOfCurrentItem = -1;
+    for (int j = 0; j < reservationsOfYear.reservations.length; j++) {
+      final Reservation currentReservation = reservationsOfYear.reservations[j];
+      if (currentReservation.departureDate.month != currentMonth) {
+        currentMonth = currentReservation.departureDate.month;
+        reservationsGroupedByMonth.add(
+          ReservationsOfMonthOfYear(
+            year: reservationsOfYear.year,
+            month: currentMonth,
+          ),
+        );
+        // indexOfCurrentItem = reservationsGroupedByMonth.length -1 ;
+      }
+      reservationsGroupedByMonth.last.reservations.add(currentReservation);
+    }
+  }
+
+  return reservationsGroupedByMonth;
+}
--- lib/analytics/graphs/revenue_per_month/yearly_monthly_revenue_breakdown_chart.dart
+++ lib/analytics/graphs/revenue_per_month/yearly_monthly_revenue_breakdown_chart.dart
@@ -1,3 +1,4 @@
+import "package:decla_time/analytics/graphs/revenue_per_month/business/get_reservations_by_month.dart";
 import "package:decla_time/analytics/graphs/taxes_per_year/business/get_reservations_by_year.dart";
 import "package:decla_time/reservations/reservation.dart";
 import "package:flutter/material.dart";
@@ -24,9 +25,11 @@ class YearlyMonthlyRevenueBreakdownChart extends StatelessWidget {
       shrinkWrap: true,
       itemBuilder: (BuildContext context, int index) {
         final List<ReservationsOfMonthOfYear> reservationsByMonthOfYear =
-            reservationsByMonth
-                .where((ReservationsOfMonthOfYear element) =>
-                    element.year == years[index])
+            getReservationsByMonth(reservationsGroupedByYear)
+                .where(
+                  (ReservationsOfMonthOfYear element) =>
+                      element.year == years[index],
+                )
                 .toList();
 
         return ListView.builder(
@@ -35,48 +38,21 @@ class YearlyMonthlyRevenueBreakdownChart extends StatelessWidget {
             final ReservationsOfMonthOfYear reservationsOfMonth =
                 reservationsByMonthOfYear[index];
 
-            return Text(DateFormat("MMMM - y: ").format(DateTime(
-                    reservationsOfMonth.year, reservationsOfMonth.month)) +
-                reservationsOfMonth.monthTotal.toStringAsFixed(2));
+            return Text(
+              DateFormat("MMMM - y: ").format(
+                    DateTime(
+                      reservationsOfMonth.year,
+                      reservationsOfMonth.month,
+                    ),
+                  ) +
+                  reservationsOfMonth.monthTotal.toStringAsFixed(2),
+            );
           },
           itemCount: reservationsByMonthOfYear.length,
         );
       },
       itemCount: years.length,
     );
-  }
-
-  ///Probably computationally expensive. Don't call it too often.
-  ///Reservations are considered sorted beforehands.
-  ///it returns a list where each element is `{year,month,List<Reservation>}`
-  List<ReservationsOfMonthOfYear> get reservationsByMonth {
-    final List<ReservationsOfMonthOfYear> reservationsGroupedByMonth =
-        <ReservationsOfMonthOfYear>[];
-
-    for (int i = 0; i < reservationsGroupedByYear.length; i++) {
-      //reservations are considered already sorted
-      final ReservationsOfYear reservationsOfYear =
-          reservationsGroupedByYear[i];
-      int currentMonth = -1;
-      // int indexOfCurrentItem = -1;
-      for (int j = 0; j < reservationsOfYear.reservations.length; j++) {
-        final Reservation currentReservation =
-            reservationsOfYear.reservations[j];
-        if (currentReservation.departureDate.month != currentMonth) {
-          currentMonth = currentReservation.departureDate.month;
-          reservationsGroupedByMonth.add(
-            ReservationsOfMonthOfYear(
-              year: reservationsOfYear.year,
-              month: currentMonth,
-            ),
-          );
-          // indexOfCurrentItem = reservationsGroupedByMonth.length -1 ;
-        }
-        reservationsGroupedByMonth.last.reservations.add(currentReservation);
-      }
-    }
-
-    return reservationsGroupedByMonth;
   }
 }
 
@@ -94,9 +70,10 @@ class ReservationsOfMonthOfYear {
   });
 
   double get monthTotal => reservations.fold(
-      0,
-      (double previousValue, Reservation reservation) =>
-          previousValue +
-          reservation.payout +
-          (reservation.cancellationAmount ?? 0));
+        0,
+        (double previousValue, Reservation reservation) =>
+            previousValue +
+            reservation.payout +
+            (reservation.cancellationAmount ?? 0),
+      );
 }
