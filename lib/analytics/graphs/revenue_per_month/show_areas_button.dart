50ea11d92c89ff3f51010daf4f96ff863bf89771
deadlyunicorn <petrache.dev@gmail.com>
2024-04-12 19:43:32 +0300

    added localizations for  average and area

--- lib/analytics/graphs/revenue_per_month/show_areas_button.dart
+++ lib/analytics/graphs/revenue_per_month/show_areas_button.dart
@@ -1,21 +1,28 @@
+import "package:decla_time/core/extensions/capitalize.dart";
 import "package:flutter/material.dart";
+
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
 
 class ShowAreasButton extends StatelessWidget {
   const ShowAreasButton({
     required this.showAreas,
     required this.setShowAreas,
+    required this.localized,
+
     super.key,
   });
 
   final bool showAreas;
   final Function(bool?) setShowAreas;
+    final AppLocalizations localized;
+
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Text("Areas"),
+        Text( localized.area.capitalized),
         Checkbox(
           value: showAreas,
           onChanged: setShowAreas,
--- lib/analytics/graphs/revenue_per_month/show_average_button.dart
+++ lib/analytics/graphs/revenue_per_month/show_average_button.dart
@@ -1,21 +1,25 @@
+import "package:decla_time/core/extensions/capitalize.dart";
 import "package:flutter/material.dart";
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
 
 class ShowAverageButton extends StatelessWidget {
   const ShowAverageButton({
     required this.showAverage,
     required this.setShowAverage,
+    required this.localized,
     super.key,
   });
 
   final bool showAverage;
   final Function(bool?) setShowAverage;
+  final AppLocalizations localized;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Text("Average"),
+        Text(localized.average.capitalized),
         Checkbox(
           value: showAverage,
           onChanged: setShowAverage,
