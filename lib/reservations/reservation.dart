a619347c1e1b2cb2b3962b999c1effdb1aaab5eb
deadlyunicorn <petrache.dev@gmail.com>
2024-04-20 01:34:09 +0300

    added a dailyRate getter and a copyWith method for <Reservation>

--- lib/reservations/reservation.dart
+++ lib/reservations/reservation.dart
@@ -7,7 +7,6 @@ part "reservation.g.dart";
 
 @collection
 class Reservation implements ItemWithDates {
-
   //TODO !! Some reservations are "CANCELLED" and show some amount ( of what would be the income if it was not cancelled)
   //TODO !! However the income is 0 - find a way to exclude those.
 
@@ -49,6 +48,37 @@ class Reservation implements ItemWithDates {
   String get arrivalDateString => DateFormat("dd/MM/y").format(arrivalDate);
   String get departureDateString => DateFormat("dd/MM/y").format(departureDate);
   Id get isarId => fastHash(id);
+
+  double get dailyRate => ((cancellationAmount ?? 0) + payout) / nights;
+
+  Reservation copyWith({
+    String? bookingPlatform,
+    String? listingName,
+    String? id,
+    String? guestName,
+    DateTime? arrivalDate,
+    DateTime? departureDate,
+    double? payout,
+    String? reservationStatus,
+    DateTime? cancellationDate,
+    double? cancellationAmount,
+    bool? isDeclared,
+    DateTime? lastEdit,
+  }) =>
+      Reservation(
+        bookingPlatform: bookingPlatform ?? this.bookingPlatform,
+        listingName: listingName ?? this.listingName,
+        id: id ?? this.id,
+        guestName: guestName ?? this.guestName,
+        arrivalDate: arrivalDate ?? this.arrivalDate,
+        departureDate: departureDate ?? this.departureDate,
+        payout: payout ?? this.payout,
+        reservationStatus: reservationStatus ?? this.reservationStatus,
+        cancellationDate: cancellationDate ?? this.cancellationDate,
+        cancellationAmount: cancellationAmount ?? this.cancellationAmount,
+        lastEdit: lastEdit ?? this.lastEdit,
+        isDeclared: isDeclared ?? this.isDeclared,
+      );
 
   bool isEqualTo(Reservation reservation) {
     return reservation.bookingPlatform == bookingPlatform &&
