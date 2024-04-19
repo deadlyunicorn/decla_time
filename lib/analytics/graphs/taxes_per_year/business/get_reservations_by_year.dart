02c2dd564b98f321c0c838d0e71bc5d3bc265b78
deadlyunicorn <petrache.dev@gmail.com>
2024-04-20 01:36:12 +0300

    split reservations on year change ( just like gov supports ) and added relevant tests.

--- lib/analytics/graphs/taxes_per_year/business/get_reservations_by_year.dart
+++ lib/analytics/graphs/taxes_per_year/business/get_reservations_by_year.dart
@@ -1,35 +1,72 @@
+import "dart:math";
+
 import "package:decla_time/core/connection/isar_helper.dart";
 import "package:decla_time/reservations/reservation.dart";
 import "package:flutter/material.dart";
 import "package:isar/isar.dart";
 import "package:provider/provider.dart";
 
+
 Future<List<ReservationsOfYear>> getReservationsByYearFuture({
   required BuildContext context,
 }) async {
   final Isar isar = await context.read<IsarHelper>().isarFuture;
-  final List<Reservation> reservations = await isar.reservations
-      .where()
-      .sortByDepartureDateDesc()
-      .findAll();
-  int year = 0;
+  final List<Reservation> reservations =
+      await isar.reservations.where().sortByDepartureDateDesc().findAll();
   final List<ReservationsOfYear> reservationsByYear = <ReservationsOfYear>[];
+
   for (int i = 0; i < reservations.length; i++) {
     final Reservation currentReservation = reservations[i];
-    if (currentReservation.departureDate.year != year) {
-      year = currentReservation.departureDate.year;
+
+    if (!reservationsByYear
+        .map((ReservationsOfYear reservationsOfYear) => reservationsOfYear.year)
+        .contains(currentReservation.departureDate.year)) {
       reservationsByYear.add(
         ReservationsOfYear(
-          year: year,
-          reservations: <Reservation>[currentReservation],
+          year: currentReservation.departureDate.year,
+          reservations: <Reservation>[],
         ),
       );
-    } else {
+    }
+
+    if (currentReservation.arrivalDate.year ==
+        currentReservation.departureDate.year) {
       reservationsByYear
-          .where((ReservationsOfYear element) => element.year == year)
+          .where(
+            (ReservationsOfYear element) =>
+                element.year == currentReservation.departureDate.year,
+          )
           .first
           .reservations
           .add(currentReservation);
+    } else {
+      final List<Reservation> splitReservations =
+          reservationSplitOnYearChange(reservation: currentReservation);
+
+      for (final Reservation splitReservation in splitReservations) {
+        if (!reservationsByYear
+            .map(
+              (ReservationsOfYear reservationsOfYear) =>
+                  reservationsOfYear.year,
+            )
+            .contains(splitReservation.departureDate.year)) {
+          reservationsByYear.add(
+            ReservationsOfYear(
+              year: splitReservation.departureDate.year,
+              reservations: <Reservation>[],
+            ),
+          );
+        }
+
+        reservationsByYear
+            .where(
+              (ReservationsOfYear element) =>
+                  element.year == splitReservation.departureDate.year,
+            )
+            .firstOrNull
+            ?.reservations
+            .add(splitReservation);
+      }
     }
   }
   return reservationsByYear;
@@ -45,4 +82,115 @@ class ReservationsOfYear {
 
   final int year;
   final List<Reservation> reservations;
+}
+
+
+//TODO - make something similar for months. 
+//? Why: Those are used for the analytics -
+//? If you leave it as is, and sb reserves for 3 months, you will get 90 days on departureDateMonth..
+
+List<Reservation> reservationSplitOnYearChange({
+  required Reservation reservation,
+}) {
+  final DateTime departureDate = reservation.departureDate;
+  final int departureYear = departureDate.year;
+
+  final int arrivalYear = reservation.arrivalDate.year;
+  final double averageDailyRate = reservation.dailyRate;
+
+  if (arrivalYear == departureYear) {
+    return <Reservation>[reservation];
+  } else {
+    final bool isCancelled = reservation.cancellationAmount != null;
+    final List<Reservation> splitReservations = <Reservation>[];
+    int hoursToAdd = DateTime(
+      reservation.arrivalDate.year + 1,
+      1,
+      1,
+      11,
+    )
+        .subtract(const Duration(days: 1))
+        .difference(
+          reservation.arrivalDate,
+        )
+        .inHours;
+
+    //* keep the arrival date.
+    Reservation currentReservation = Reservation(
+      bookingPlatform: reservation.bookingPlatform,
+      listingName: reservation.listingName,
+      id: reservation.id,
+      guestName: reservation.guestName,
+      arrivalDate: reservation.arrivalDate,
+      departureDate: reservation.arrivalDate.add(
+        Duration(
+          hours: hoursToAdd,
+        ),
+      ),
+      payout: isCancelled
+          ? 0
+          : Duration(hours: hoursToAdd).inDays * averageDailyRate +
+              averageDailyRate,
+      reservationStatus: reservation.reservationStatus,
+      cancellationDate: reservation.cancellationDate,
+      cancellationAmount: isCancelled
+          ? Duration(hours: hoursToAdd).inDays * averageDailyRate +
+              averageDailyRate
+          : null,
+    );
+    splitReservations.add(currentReservation);
+
+    for (int currentYear = arrivalYear + 1;
+        currentYear <= departureYear;
+        currentYear++) {
+      final DateTime currentArrivalDate = DateTime(currentYear, 1, 1, 13);
+      hoursToAdd = min(
+        DateTime(
+          currentYear + 1,
+          1,
+          1,
+          11,
+        )
+            .subtract(const Duration(days: 1))
+
+            // .subtract(const Duration(days: 1))
+            .difference(
+              currentArrivalDate,
+            )
+            .inHours,
+        reservation.departureDate
+            .difference(
+              currentArrivalDate,
+            )
+            .inHours,
+      );
+
+      currentReservation = Reservation(
+        bookingPlatform: reservation.bookingPlatform,
+        listingName: reservation.listingName,
+        id: reservation.id,
+        guestName: reservation.guestName,
+        arrivalDate: currentArrivalDate,
+        departureDate: currentArrivalDate.add(
+          Duration(
+            hours: hoursToAdd,
+          ),
+        ),
+        payout: isCancelled
+            ? 0
+            : Duration(hours: hoursToAdd).inDays.round() * averageDailyRate +
+                2 * averageDailyRate,
+        reservationStatus: reservation.reservationStatus,
+        cancellationDate: reservation.cancellationDate,
+        cancellationAmount: isCancelled
+            ? Duration(hours: hoursToAdd).inDays.round() * averageDailyRate +
+                2 * averageDailyRate
+            : null,
+      );
+
+      splitReservations.add(currentReservation);
+    }
+
+    return splitReservations;
+  }
 }
---
+++ tests/analytics/reservations_by_year.test.dart
@@ -0,0 +1,115 @@
+import "package:decla_time/analytics/graphs/taxes_per_year/business/get_reservations_by_year.dart";
+import "package:decla_time/reservations/reservation.dart";
+import "package:test/test.dart";
+
+void main() {
+  test("Edge cases for grouping reservations by year", () {
+    final Reservation reservation = Reservation(
+      bookingPlatform: "Airbnb",
+      listingName: null,
+      id: "123123",
+      guestName: "haha",
+      arrivalDate: DateTime(2021, 04, 02),
+      departureDate: DateTime(2024, 02, 01, 11),
+      payout: 2400,
+      reservationStatus: "completed",
+      cancellationDate: null,
+      cancellationAmount: null,
+    );
+
+    final List<Reservation> splitReservations = reservationSplitOnYearChange(
+      reservation: reservation,
+    );
+
+    expect(splitReservations.first.departureDate, DateTime(2021, 12, 31, 11));
+
+    expect(splitReservations[1].departureDate, DateTime(2022, 12, 31, 11));
+    expect(splitReservations[2].arrivalDate, DateTime(2023, 1, 1, 13));
+    expect(splitReservations[2].departureDate, DateTime(2023, 12, 31, 11));
+    expect(splitReservations[3].departureDate, DateTime(2024, 02, 01, 11));
+
+    final int totalNightsOfSplit = splitReservations.fold<int>(
+      0,
+      (int previousValue, Reservation element) =>
+          previousValue + element.nights,
+    );
+
+    expect(
+      totalNightsOfSplit,
+      //! We lose one night per year..
+      reservation.nights -
+          (reservation.departureDate
+                      .difference(reservation.arrivalDate)
+                      .inDays /
+                  365)
+              .round(),
+    );
+
+    final double totalPayoutOfSplit = splitReservations.fold<double>(
+      0,
+      (double previousValue, Reservation element) =>
+          previousValue + element.payout,
+    );
+    expect(totalPayoutOfSplit, reservation.payout);
+
+    expect(
+      splitReservations.first.payout.toStringAsFixed(2),
+      (reservation.dailyRate *
+              DateTime(reservation.arrivalDate.year + 1)
+                  .difference(reservation.arrivalDate)
+                  .inDays)
+          .toStringAsFixed(2),
+    );
+  });
+
+  test("Edge cases for grouping reservations by year, 2011", () {
+    final Reservation reservation = Reservation(
+      bookingPlatform: "Airbnb",
+      listingName: null,
+      id: "123123",
+      guestName: "haha",
+      arrivalDate: DateTime(2011, 04, 02),
+      departureDate: DateTime(2024, 02, 01, 11),
+      payout: 2400,
+      reservationStatus: "completed",
+      cancellationDate: null,
+      cancellationAmount: null,
+    );
+
+    final List<Reservation> splitReservations = reservationSplitOnYearChange(
+      reservation: reservation,
+    );
+
+    final int totalNightsOfSplit = splitReservations.fold<int>(
+      0,
+      (int previousValue, Reservation element) =>
+          previousValue + element.nights,
+    );
+
+    expect(
+      totalNightsOfSplit,
+      reservation.nights -
+          (reservation.departureDate
+                      .difference(reservation.arrivalDate)
+                      .inDays /
+                  365)
+              .round(),
+    );
+
+    final double totalPayoutOfSplit = splitReservations.fold<double>(
+      0,
+      (double previousValue, Reservation element) =>
+          previousValue + element.payout,
+    );
+    expect(totalPayoutOfSplit, reservation.payout);
+
+    expect(
+      splitReservations.first.payout.toStringAsFixed(2),
+      (reservation.dailyRate *
+              DateTime(reservation.arrivalDate.year + 1)
+                  .difference(reservation.arrivalDate)
+                  .inDays)
+          .toStringAsFixed(2),
+    );
+  });
+}
