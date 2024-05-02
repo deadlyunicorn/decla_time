057429b253a9ed30dcb0e9ce686805196cc4fedf
deadlyunicorn <petrache.dev@gmail.com>
2024-05-02 14:53:55 +0300

    added the ability to select reservations place in analytics. + localizations

--- lib/analytics/analytics_page.dart
+++ lib/analytics/analytics_page.dart
@@ -2,16 +2,25 @@ import "package:decla_time/analytics/app_settings.dart";
 import "package:decla_time/analytics/graphs/analytics_graphs.dart";
 import "package:decla_time/core/extensions/capitalize.dart";
 import "package:decla_time/core/widgets/column_with_spacings.dart";
+import "package:decla_time/declarations_action_button_menu/filtering_reservations/reservation_place_selector.dart";
+import "package:decla_time/reservations/reservation_place.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
 
-class AnalyticsPage extends StatelessWidget {
+class AnalyticsPage extends StatefulWidget {
   const AnalyticsPage({
     required this.localized,
     super.key,
   });
 
   final AppLocalizations localized;
+
+  @override
+  State<AnalyticsPage> createState() => _AnalyticsPageState();
+}
+
+class _AnalyticsPageState extends State<AnalyticsPage> {
+  ReservationPlace? selectedReservationPlace;
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +31,50 @@ class AnalyticsPage extends StatelessWidget {
           spacing: 64,
           children: <Widget>[
             Text(
-              localized.analytics.capitalized,
+              widget.localized.analytics.capitalized,
               style: Theme.of(context).textTheme.headlineLarge,
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
             ),
-            AnalyticsGraphs(localized: localized),
-            AppSettings(localized: localized),
+            ColumnWithSpacings(
+              spacing: 8,
+              children: <Widget>[
+                Center(
+                  child: ColumnWithSpacings(
+                    spacing: 16,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "${widget.localized.displayAnalyticsFor.capitalized}:",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      ReservationPlaceSelector(
+                        setSelectedReservationPlace:
+                            setSelectedReservationPlace,
+                        selectedReservationPlace: selectedReservationPlace,
+                        localized: widget.localized,
+                      ),
+                    ],
+                  ),
+                ),
+                AnalyticsGraphs(
+                  localized: widget.localized,
+                  selectedReservationPlace: selectedReservationPlace,
+                ),
+              ],
+            ),
+            AppSettings(localized: widget.localized),
           ],
         ),
       ),
     );
+  }
+
+  void setSelectedReservationPlace(ReservationPlace? newPlace) {
+    if (newPlace != selectedReservationPlace) {
+      setState(() {
+        selectedReservationPlace = newPlace;
+      });
+    }
   }
 }
--- lib/analytics/graphs/analytics_graphs.dart
+++ lib/analytics/graphs/analytics_graphs.dart
@@ -6,8 +6,10 @@ import "package:decla_time/analytics/graphs/revenue_per_month/yearly_monthly_rev
 import "package:decla_time/analytics/graphs/taxes_per_year/business/get_reservations_by_year.dart";
 import "package:decla_time/analytics/graphs/taxes_per_year/taxes_per_year_pies.dart";
 import "package:decla_time/core/connection/isar_helper.dart";
+import "package:decla_time/core/extensions/capitalize.dart";
 import "package:decla_time/core/widgets/column_with_spacings.dart";
 import "package:decla_time/reservations/reservation.dart";
+import "package:decla_time/reservations/reservation_place.dart";
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
 import "package:isar/isar.dart";
@@ -16,15 +18,20 @@ import "package:provider/provider.dart";
 class AnalyticsGraphs extends StatelessWidget {
   const AnalyticsGraphs({
     required this.localized,
+    required this.selectedReservationPlace,
     super.key,
   });
 
   final AppLocalizations localized;
+  final ReservationPlace? selectedReservationPlace;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Reservation>>(
-      future: getAllReservationsFromDatabaseFuture(context: context),
+      future: getReservationFromDatabaseWhere(
+        context: context,
+        selectedReservationPlaceId: selectedReservationPlace?.id,
+      ),
       builder: (
         BuildContext context,
         AsyncSnapshot<List<Reservation>?> snapshot,
@@ -37,43 +44,55 @@ class AnalyticsGraphs extends StatelessWidget {
             snapshot.data != null
                 ? getReservationsByMonthForAnalytics(reservationsByYear)
                 : <ReservationsOfMonthOfYear>[];
-        return ColumnWithSpacings(
-          spacing: 16,
-          children: <Widget>[
-            YearlyMonthlyRevenueBreakdownChart(
-              localized: localized,
-              reservationsByMonthByYear: reservationsByMonthByYear,
-            ),
-            // * Moved inside the YearlyMonthlyRevenueBreadkownChart
-            // * as an optional chart.
-            // DaysFilledPerMonthChart(
-            //   localized: localized,
-            //   reservationsByMonthByYear: reservationsByMonthByYear,
-            // ),
+        return snapshot.data == null || snapshot.data!.length < 10
+            ? Padding(
+                padding: const EdgeInsets.only(top: 32.0),
+                child: Center(child: Text(localized.noEnoughData.capitalized)),
+              )
+            : ColumnWithSpacings(
+                spacing: 16,
+                children: <Widget>[
+                  YearlyMonthlyRevenueBreakdownChart(
+                    localized: localized,
+                    reservationsByMonthByYear: reservationsByMonthByYear,
+                  ),
+                  // * Moved inside the YearlyMonthlyRevenueBreadkownChart
+                  // * as an optional chart.
+                  // DaysFilledPerMonthChart(
+                  //   localized: localized,
+                  //   reservationsByMonthByYear: reservationsByMonthByYear,
+                  // ),
 
-            TaxesPerYearPies(
-              localized: localized,
-              reservationsGroupedByYear: reservationsByYear,
-            ),
+                  TaxesPerYearPies(
+                    localized: localized,
+                    reservationsGroupedByYear: reservationsByYear,
+                  ),
 
-            PlatformSharePie(
-              localized: localized,
-              reservations: snapshot.data ?? <Reservation>[],
-            ),
-            PerformanceComparedToLastYear(
-              localized: localized,
-              reservationsByMonthByYear: reservationsByMonthByYear,
-            ),
-          ],
-        );
+                  PlatformSharePie(
+                    localized: localized,
+                    reservations: snapshot.data ?? <Reservation>[],
+                  ),
+                  PerformanceComparedToLastYear(
+                    localized: localized,
+                    reservationsByMonthByYear: reservationsByMonthByYear,
+                  ),
+                ],
+              );
       },
     );
   }
 
-  Future<List<Reservation>> getAllReservationsFromDatabaseFuture({
+  Future<List<Reservation>> getReservationFromDatabaseWhere({
+    required int? selectedReservationPlaceId,
     required BuildContext context,
   }) async {
     final Isar isar = await context.read<IsarHelper>().isarFuture;
-    return await isar.reservations.where().sortByDepartureDateDesc().findAll();
+
+    return selectedReservationPlace == null
+        ? isar.reservations.where().findAll()
+        : isar.reservations
+            .filter()
+            .reservationPlaceIdEqualTo(selectedReservationPlaceId)
+            .findAll();
   }
 }
